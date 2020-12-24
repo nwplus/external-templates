7fcad9acae93250b720f61516f93fca28a28b955
kozr <zxzflow@gmail.com>
2020-12-23 19:11:23 -0800

    add firebase connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import Head from 'next/head'
+import fireDb from '@utilities/firebase'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [targetWebsite, setTargetWebsite] = useState('Hello world')
+  useEffect(() => {
+    const getBuildTarget = async () => {
+      const targetWebsite = await fireDb.getTargetedHackathon()
+      setTargetWebsite(targetWebsite)
+    }
+    getBuildTarget()
+  }, [])
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello world</h1>
+      <h1>{targetWebsite}</h1>
 
       <footer>
         <a
