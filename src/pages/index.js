52d508666e727472f2cc78fe3d8c03a5025e2055
kozr <zxzflow@gmail.com>
2021-01-03 01:27:54 -0800

    set up list rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import GlobalStyles from '@styles/global'
 import fireDb from '@utilities/firebase'
+import FAQExpandable from '@components/faqTemplates/faq_category_expandable'
 
 export default function Index(props) {
   return (
@@ -11,8 +12,9 @@ export default function Index(props) {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FAQExpandable faq={props.faq} />
       <h1>Website</h1>
-      <p>This is a paragraph and here are some flags for example: {props.faqs}</p>
+      <p>This is a paragraph and here are some flags for example: {props.flags}</p>
     </div>
   )
 }
@@ -32,12 +34,12 @@ export async function getStaticProps(context) {
   const websiteData = await fireDb.getWebsiteData(targetedHackathon)
 
   const { FeatureFlags } = websiteData
-  const faqs = await fireDb.getCollection(targetedHackathon, 'FAQ')
-
+  const faq = await fireDb.getCollection(targetedHackathon, 'FAQ')
+  console.log(faq)
   return {
     props: {
       flags: JSON.stringify(FeatureFlags),
-      faqs: JSON.stringify(faqs),
+      faq: JSON.stringify(faq),
     }, // will be passed to the page component as props
   }
 }
