c7515c9d7a842bd0d10a5fab444e5636998b7add
Ryan Mehri <ryan.mehri1@gmail.com>
2021-01-21 17:57:37 -0700

    Make navbar fade in/out on scroll

--- src/components/hero/NavBar.jsx
+++ src/components/hero/NavBar.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import logo from '@assets/logo.png'
 import { Columns, Column, SectionContainer } from '@lib/Containers'
+import { debounce } from '@lib/Helpers'
 
 const Nav = styled.nav`
     position: fixed;
     width: 100vw;
-    visibility: visible;
+    visibility: ${p => p.visible ? 'visible' : 'hidden'};
+    opacity: ${p => p.visible ? 1 : 0};
     transition: 0.5s ease-in-out;
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
     align-items: center;
     margin-right: 80px;
+`
 
-    & > a {
-      color: ${p => p.fontColor};
-    }
+const NavItem = styled.a`
+    font-size: 18px;
+    padding: 0px 32px;
+    text-decoration: none;
 
-    & > a::after {
+    color: ${p => p.fontColor};
+
+    &::after {
       content: "";
       display: block;
       width: 0;
@@ -27,22 +33,16 @@ const Nav = styled.nav`
       background: ${p => p.fontColor};
     }
 
-    & > a:hover::after {
+    &:hover::after {
       width: 100%;
     }
 
-    & > a:hover,
-    & > a:focus,
-    & > a:focus-within {
+    &:hover,
+    &:focus,
+    &:focus-within {
       background: none;
       color: ${p => p.fontColor};
     }
-`
-
-const NavItem = styled.a`
-    font-size: 18px;
-    padding: 0px 32px;
-    text-decoration: none;
 `
 
 const Logo = styled.img`
@@ -51,22 +51,40 @@ const Logo = styled.img`
 `
 
 const Navbar = ({ config, flags }) => {
-    const { fontColor, previousYearLink } = config
-    const { faqFlag, sponsorFlag } = flags
-    return (
-        <Nav fontColor={fontColor} role="navigation" aria-label="main navigation">
-            <NavItem href="#about">About</NavItem>
-            {faqFlag && <NavItem href="#faq">FAQ</NavItem>}
-            {sponsorFlag && <NavItem href="#sponsors">Sponsors</NavItem>}
-            <NavItem href={previousYearLink}>Last Year</NavItem>
-            <a href="https://www.nwplus.io/" target="_blank" rel="noopener">
-                <Logo
-                    src="/nwplus-logo.png"
-                    alt="nwPlus logo"
-                />
-            </a>
-        </Nav>
-    )
+  const { fontColor, previousYearLink } = config
+  const { faqFlag, sponsorFlag } = flags
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset
+
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10)
+
+    setPrevScrollPos(currentScrollPos)
+  }, 100)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [prevScrollPos, visible, handleScroll])
+
+  return (
+    <Nav visible={visible} role="navigation" aria-label="main navigation">
+      <NavItem href="#about" fontColor={fontColor}>About</NavItem>
+      {faqFlag && <NavItem href="#faq" fontColor={fontColor}>FAQ</NavItem>}
+      {sponsorFlag && <NavItem href="#sponsors" fontColor={fontColor}>Sponsors</NavItem>}
+      <NavItem href={previousYearLink} fontColor={fontColor}>Last Year</NavItem>
+      <a href="https://www.nwplus.io/" target="_blank" rel="noopener">
+        <Logo
+          src="/nwplus-logo.png"
+          alt="nwPlus logo"
+        />
+      </a>
+    </Nav>
+  )
 }
 
 export default Navbar
