bcc8bb64e12b616288f60eb66a243dd86e7c6a67
Ryan Mehri <ryan.mehri1@gmail.com>
2021-02-06 20:18:31 -0700

    Format with prettier

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,29 +4,29 @@ import { SectionContainer } from '@lib/Containers'
 import Button from './Button'
 
 const Title = styled.p`
-    text-align: center;
+  text-align: center;
 `
 
 const HeroContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  margin: auto;
 `
 
 const Hero = ({ hero }) => {
-    const { buttonText, applyActive } = hero
-    return (
-        <SectionContainer>
-            <HeroContainer>
-                <Button enabled={applyActive}>{buttonText}</Button>
-                <p>March 2021</p>
-            </HeroContainer>
-        </SectionContainer>
-    )
+  const { buttonText, applyActive } = hero
+  return (
+    <SectionContainer>
+      <HeroContainer>
+        <Button enabled={applyActive}>{buttonText}</Button>
+        <p>March 2021</p>
+      </HeroContainer>
+    </SectionContainer>
+  )
 }
 
 export default Hero
