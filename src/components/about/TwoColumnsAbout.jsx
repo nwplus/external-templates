2eee91d9ffc299b18401d882b272e2fd8fc70f3e
Alice Kim <aliceykim0828@gmail.com>
2021-02-09 13:28:02 -0800

    implement About section mobile friendly

--- src/components/about/TwoColumnsAbout.jsx
+++ src/components/about/TwoColumnsAbout.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '@assets/logo__cmdf.png'
 import background from '@assets/about__bg.svg'
-import { LAPTOP, scale } from '@constants/measurements'
+import mobile from '@assets/about__bg_sm.svg'
+import { LAPTOP, TABLET, scale } from '@constants/measurements'
 import { SectionContainerWithBackground as Base } from '@lib/Containers'
 
 const SectionContainer = styled(Base)`
     @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@700&display=swap');
 
     font-size: ${() => scale(320, 1440, 6, 28)};
+    position: relative;
     text-align: center;
     height: 93vw;
     .intro {
@@ -41,6 +43,30 @@ const SectionContainer = styled(Base)`
         div {
             width: inherit;
             margin: 2vw;
+        }
+    }
+    @media (max-width: ${TABLET}) {
+        background-image: url(${mobile});
+        background-position: top;
+        height: 285vw;
+        .intro {
+            padding: 97vw 0 0;
+            font-size: ${() => scale(320, 786, 14, 32)};
+        }
+        .copy {
+            padding-top: 79vw;
+            font-size: ${() => scale(320, 786, 14, 36)};
+            text-align: left;
+        }
+        .countdown {
+            position: absolute;
+            top: 147vw;
+            left: 54vw;
+            font-size: 54px;
+            font-size: ${() => scale(320, 786, 22, 54)};
+        }
+        img {
+            display: none;
         }
     }
 `
