ecca0e81e1f2ded3d14537d3c45d96366b60bff7
andykwan1124 <woody.andygrant@gmail.com>
2021-02-10 22:38:13 -0800

    B)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withOptimizedImages = require('next-optimized-images')
 
 module.exports = () => {
-  if (process.env.DEPLOY_ENV !== 'PRODUCTION') {
+  if (process.env.DEPLOY_ENV !== 'PRODUCTION')
     return withOptimizedImages({
       reactStrictMode: true,
       poweredByHeader: false,
@@ -16,19 +16,17 @@ module.exports = () => {
         NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_ENV_FIREBASE_APP_ID,
       },
     })
-  } else {
-    return withOptimizedImages({
-      poweredByHeader: false,
-      env: {
-        NEXT_PUBLIC_FIREBASE_API_KEY: 'AIzaSyDGa7alU0NhfBATSQ6CalkY4Za9wWPrM7o',
-        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: 'nwplus-ubc.firebaseapp.com',
-        NEXT_PUBLIC_FIREBASE_DATABASE_URL: 'https://nwplus-ubc.firebaseio.com',
-        NEXT_PUBLIC_FIREBASE_PROJECT_ID: 'nwplus-ubc',
-        NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'nwplus-ubc.appspot.com',
-        NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: 'G-BT2W7DSL1G',
-        NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: '306881258768',
-        NEXT_PUBLIC_FIREBASE_APP_ID: '1:306881258768:web:bc922148732abee79f7195',
-      },
-    })
-  }
+  return withOptimizedImages({
+    poweredByHeader: false,
+    env: {
+      NEXT_PUBLIC_FIREBASE_API_KEY: 'AIzaSyDGa7alU0NhfBATSQ6CalkY4Za9wWPrM7o',
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: 'nwplus-ubc.firebaseapp.com',
+      NEXT_PUBLIC_FIREBASE_DATABASE_URL: 'https://nwplus-ubc.firebaseio.com',
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID: 'nwplus-ubc',
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'nwplus-ubc.appspot.com',
+      NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: 'G-BT2W7DSL1G',
+      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: '306881258768',
+      NEXT_PUBLIC_FIREBASE_APP_ID: '1:306881258768:web:bc922148732abee79f7195',
+    },
+  })
 }
