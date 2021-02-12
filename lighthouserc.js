35f0e32ec119c3707c96aaf2904e54cfee51c1ad
andykwan1124 <woody.andygrant@gmail.com>
2021-02-12 00:26:27 -0800

    add lighthouserc

---
+++ lighthouserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    assert: {
+      preset: 'lighthouse:recommended',
+      // assertions: {
+      //   'apple-touch-icon': 'off',
+      //   'html-has-lang': 'off',
+      //   'installable-manifest': 'off',
+      //   'maskable-icon-': 'off',
+      //   'apple-touch-icon': 'off',
+      //   'apple-touch-icon': 'off',
+      //   'apple-touch-icon': 'off',
+      //   'apple-touch-icon': 'off',
+      //   'apple-touch-icon': 'off',
+      //   'apple-touch-icon': 'off',
+      // },
+    },
+  },
+};
