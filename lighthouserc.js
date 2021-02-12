b092361f1304a0ebb50e1764fd51ad734a9f3a3d
andykwan1124 <woody.andygrant@gmail.com>
2021-02-12 00:31:22 -0800

    turned off 11 assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,18 +2,18 @@ module.exports = {
   ci: {
     assert: {
       preset: 'lighthouse:recommended',
-      // assertions: {
-      //   'apple-touch-icon': 'off',
-      //   'html-has-lang': 'off',
-      //   'installable-manifest': 'off',
-      //   'maskable-icon-': 'off',
-      //   'apple-touch-icon': 'off',
-      //   'apple-touch-icon': 'off',
-      //   'apple-touch-icon': 'off',
-      //   'apple-touch-icon': 'off',
-      //   'apple-touch-icon': 'off',
-      //   'apple-touch-icon': 'off',
-      // },
+      assertions: {
+        'apple-touch-icon': 'off',
+        'html-has-lang': 'off',
+        'installable-manifest': 'off',
+        'maskable-icon-audit': 'off',
+        'meta-description': 'off',
+        'offline-start-url': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+        'works-offline': 'off',
+      },
     },
   },
 };
