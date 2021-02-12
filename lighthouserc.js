5bc11408d2e392cabdce248bc7e1f2b2daa45ede
andykwan1124 <woody.andygrant@gmail.com>
2021-02-12 00:48:48 -0800

    added upload target

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,20 @@
 module.exports = {
+  upload: {
+    target: 'temporary-public-storage',
+  },
   ci: {
     assert: {
       assertions: {
+        'apple-touch-icon': 'off',
+        'html-has-lang': 'off',
+        'installable-manifest': 'off',
+        'maskable-icon-audit': 'off',
+        'meta-description': 'off',
+        'offline-start-url': 'off',
+        'service-worker': 'off',
+        'splash-screen': 'off',
+        'themed-omnibox': 'off',
+        'works-offline': 'off',
       },
     },
   },
