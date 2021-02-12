4b0af3ac13e3b24b3d0ab1889f2f41f177738d56
andykwan1124 <woody.andygrant@gmail.com>
2021-02-12 00:45:38 -0800

    remove all assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,16 +2,6 @@ module.exports = {
   ci: {
     assert: {
       assertions: {
-        'apple-touch-icon': 'off',
-        'html-has-lang': 'off',
-        'installable-manifest': 'off',
-        'maskable-icon-audit': 'off',
-        'meta-description': 'off',
-        'offline-start-url': 'off',
-        'service-worker': 'off',
-        'splash-screen': 'off',
-        'themed-omnibox': 'off',
-        'works-offline': 'off',
       },
     },
   },
