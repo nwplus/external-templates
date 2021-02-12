398a0e86c758a3fde8cfbd756a43b1c0f9ea1a02
Ryan Mehri <ryan.mehri1@gmail.com>
2021-02-12 10:37:04 -0700

    Fix debounce linting errors

--- src/lib/Helpers.jsx
+++ src/lib/Helpers.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-rest-params */
 import styled from 'styled-components'
 
 // Add spacing between components
@@ -7,21 +6,32 @@ export const Spacers = styled.div`
   height: ${p => p.height ?? '0px'};
 `
 
-// Limits the rate at which a function can be called
-// Avoids too many renders on scroll for example
-export function debounce(func, wait, immediate) {
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// `wait` milliseconds.
+export const debounce = (func, wait) => {
   let timeout
 
-  return function () {
-    const context = this
-    const args = arguments
-    const later = function () {
+  // This is the function that is returned and will be executed many times
+  // We spread (...args) to capture any number of parameters we want to pass
+  return function executedFunction(...args) {
+    // The callback function to be executed after
+    // the debounce time has elapsed
+    const later = () => {
+      // null timeout to indicate the debounce ended
       timeout = null
-      if (!immediate) func.apply(context, args)
+
+      // Execute the callback
+      func(...args)
     }
-    const callNow = immediate && !timeout
+    // This will reset the waiting every function execution.
+    // This is the step that prevents the function from
+    // being executed because it will never reach the
+    // inside of the previous setTimeout
     clearTimeout(timeout)
+
+    // Restart the debounce waiting period.
+    // setTimeout returns a truthy value (it differs in web vs Node)
     timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
   }
 }
