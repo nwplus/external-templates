937d4aa75280fc7cc24533193af8ec3178d3d301
Ryan Mehri <ryan.mehri1@gmail.com>
2021-02-13 12:20:14 -0700

    Fix linting errors

--- src/lib/Helpers.jsx
+++ src/lib/Helpers.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
 // Add spacing between components
-export const Spacers = styled.div`
+const Spacers = styled.div`
   width: ${p => p.width ?? '0px'};
   height: ${p => p.height ?? '0px'};
 `
+
+export default Spacers
