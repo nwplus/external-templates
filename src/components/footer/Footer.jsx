2455dff4bc45e90b73e222973112ac2aab5f2f86
unknown <ianmmah@gmail.com>
2021-02-13 15:27:16 -0800

    Add computer icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,25 +5,43 @@ import icon_fb from '@assets/icon_fb.svg'
 import icon_twitter from '@assets/icon_twitter.svg'
 import icon_insta from '@assets/icon_insta.svg'
 import icon_medium from '@assets/icon_medium.svg'
-import { SectionContainerWithBackground as Base, SectionContainer as Container } from '@lib/Containers'
+import footer_icons from '@assets/footer_icons.svg'
+import { SectionContainerWithBackground as Base } from '@lib/Containers'
+
+const Container = styled(Base)`
+    text-align: center;
+`
 
 const StyledA = styled.a`
     color: #B95D1D;
     margin: 3em 1em;
 `
 
+const Icons = styled.img`
+    margin: 3em 0;
+    max-width: 90vw;
+`
+
 const StyledIcon = styled.img`
     margin: 1em;
 `
 
-const Footer = ({ test }) => {
+const Footer = () => {
     return (
-        <Base>
+        <Container>
             <Window title="footer.png">
-                <StyledIcon src={icon_fb} alt="Facebook"/>
-                <StyledIcon src={icon_twitter} alt="Facebook"/>
-                <StyledIcon src={icon_insta} alt="Facebook"/>
-                <StyledIcon src={icon_medium} alt="Facebook"/>
+                <a href="https://www.facebook.com/nwplusubc/">
+                    <StyledIcon src={icon_fb} alt="Facebook"/>
+                </a>
+                <a href="https://twitter.com/nwplusubc">
+                <StyledIcon src={icon_twitter} alt="Twitter"/>
+                </a>
+                <a href="https://www.instagram.com/nwplusubc/">
+                <StyledIcon src={icon_insta} alt="Instagram"/>
+                </a>
+                <a href="https://medium.com/nwplusubc">
+                <StyledIcon src={icon_medium} alt="Medium"/>
+                </a>
                 <br />
                 <StyledA href="mailto: test@nwplus.io">E-mail Us</StyledA>
                 <StyledA href="http://google.com">Code of Conduct</StyledA>
@@ -31,7 +49,9 @@ const Footer = ({ test }) => {
                 <p>Organized and held by nwPlus</p>
                 <p>Copyright © cmd-f 2021</p>
             </Window>
-        </Base>
+
+            <Icons src={footer_icons} alt="Computer icons" />
+        </Container>
     )
 }
 
