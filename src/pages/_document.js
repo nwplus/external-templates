a7c54c4e7676eefb2830e00c5eaefb1f8a068e56
andykwan1124 <woody.andygrant@gmail.com>
2021-02-13 21:14:02 -0800

    change back to getInitial

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getStaticProps(ctx) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
             sheet.collectStyles(<App {...props} />),
         });
 
-      const initialProps = await Document.getStaticProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
