27159521944dcda1875d87a7cc9c502934999313
kozr <zxzflow@gmail.com>
2021-03-10 20:19:01 -0800

    refactor non website content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
+import GlobalStyles from '@styles/global'
+import Header from '@components/Header'
 /* eslint react/jsx-filename-extension: 0 */
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
