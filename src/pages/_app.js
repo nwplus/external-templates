deff84497f1a654a26a1726d82c3d41e418b604e
Jacky Zhao <j.zhao2k19@gmail.com>
2021-10-16 22:25:30 -0700

    fix linting errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
-import GlobalStyles from "../theme/GlobalStyle";
+import GlobalStyle from "../theme/GlobalStyle";
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import ThemeProvider from "../theme/ThemeProvider";
 import '../theme/fonts/style.css'
 
+/* eslint react/jsx-filename-extension: 0 */
+
 function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <GlobalStyles />
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   )
