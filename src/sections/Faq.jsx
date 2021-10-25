5c4069ebedd5245a6bd54aa8d68bec8794295df5
Kevin Zou <kevin.zp.zou@gmail.com>
2021-10-25 16:51:35 -0700

    Nit

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
   grid-column: 3 / span 10;
 `
 
-export default function FAQ() {
+export default function Faq() {
   return (
     <BgSectionContainer>
       <Wrapper>
