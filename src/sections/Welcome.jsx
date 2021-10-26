eb2b373f4604bc5bdb922746d04d2848b6768d50
Kevin Zou <kevin.zp.zou@gmail.com>
2021-10-25 23:59:49 -0700

    Add transition section

--- src/sections/Welcome.jsx
+++ src/sections/Welcome.jsx
@@ -3,16 +3,26 @@ import styled from 'styled-components';
 import { SectionContainer } from '@lib/Containers';
 import { Body, Header1 } from '@components/Typography';
 
+const Transition = styled.img`
+  position: absolute;
+  width: 100%;
+  margin-top: -6vw;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    margin-top: -10vw;
+  }
+`
+
 const BgSectionContainer = styled(SectionContainer)`
-  background: url('assets/background/welcome/welcome_2.png') center bottom, url('assets/background/welcome/welcome_1.png') center top, radial-gradient(82.82% 82.82% at 50% 94%, rgba(87, 112, 121, 0) 0%, rgba(46, 30, 85, 0.5) 100%), #A1C1D9;
+  background: url('assets/background/welcome/welcome_2.png') center bottom, url('assets/background/welcome/welcome_1.png') center 28vw, radial-gradient(82.82% 82.82% at 50% 94%, rgba(87, 112, 121, 0) 0%, rgba(46, 30, 85, 0.5) 100%), #A1C1D9;
   background-size: 100vw;
   background-repeat: no-repeat;
   height: min-content;
-  padding: 20% 0;
+  padding: 40vw 0 20%;
 
   ${p => p.theme.mediaQueries.mobile} {
     background: radial-gradient(82.82% 82.82% at 50% 94%, rgba(87, 112, 121, 0) 0%, rgba(46, 30, 85, 0.5) 100%), #A1C1D9;
-    padding: 3.625rem 0 0;
+    padding: 32vw 0 0;
     text-align: center;
   }
 `
@@ -43,20 +53,23 @@ export default function Welcome() {
   const mobileBreakpoint = 768;
 
   return (
-    <BgSectionContainer>
-      <GridContainer>
-        {windowWidth <= mobileBreakpoint ?
-          <div>
-            <div>Welcome to</div>
-            <Header1 id="about">HackCamp</Header1>
-          </div>
-          :
-          <Header1 id="about">Welcome to HackCamp</Header1>
-        }
-        <Body>HackCamp revolves around inclusivity, diversity, and accessibility — we want you to bring your unique perspectives and experiences to the table!</Body>
-        <Body>Over the past 4 years, HackCamp, or formerly UBC Local Hack Day, has been focused on encouraging beginners and people who are curious about technology to work on a project that focuses on these three main pillars.</Body>
-      </GridContainer>
-      <Image src="/assets/background/welcome/welcome_mobile.png" />
-    </BgSectionContainer>
+    <>
+      <Transition src="/assets/background/welcome/transition.png" />
+      <BgSectionContainer>
+        <GridContainer>
+          {windowWidth <= mobileBreakpoint ?
+            <div>
+              <div>Welcome to</div>
+              <Header1 id="about">HackCamp</Header1>
+            </div>
+            :
+            <Header1 id="about">Welcome to HackCamp</Header1>
+          }
+          <Body>HackCamp revolves around inclusivity, diversity, and accessibility — we want you to bring your unique perspectives and experiences to the table!</Body>
+          <Body>Over the past 4 years, HackCamp, or formerly UBC Local Hack Day, has been focused on encouraging beginners and people who are curious about technology to work on a project that focuses on these three main pillars.</Body>
+        </GridContainer>
+        <Image src="/assets/background/welcome/welcome_mobile.png" />
+      </BgSectionContainer>
+    </>
   )
 }
