759ab7939d0f3223762f1249050c4e0752a0d815
Kevin Zou <kevin.zp.zou@gmail.com>
2021-10-25 21:07:12 -0700

    Fixups

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ export default function Index({ title }) {
         {/* Remove comment once favicon is set */}
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta itemProp="image" content="/assets/meta-image.png" />
+        <meta itemProp="image" content="/meta-image.png" />
         {/* Remove comment once description is written */}
         <meta
           name="description"
