2097174b8f8efeed512421dc29c8ecf664a2fb54
Kevin Zou <kevin.zp.zou@gmail.com>
2021-10-25 17:41:51 -0700

    Transition from FAQ

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -4,19 +4,29 @@ import FaqCollection from "@components/FaqCollection";
 
 const BgSectionContainer = styled(SectionContainer)`
   background: url('assets/background/faq/cave.png') center/100vw no-repeat, linear-gradient(180deg, #5695AC 0%, #002B52 100%);
-  padding: 6rem 0;
+  min-height: 1200px;
+  padding: 6rem 0 0;
 `
 
 const Wrapper = styled.div`
   grid-column: 3 / span 10;
 `
 
+const Image = styled.img`
+  width: 100vw;
+  pointer-events: none;
+  user-select: none;
+`
+
 export default function Faq() {
   return (
-    <BgSectionContainer>
-      <Wrapper>
-        <FaqCollection />
-      </Wrapper>
-    </BgSectionContainer>
+    <>
+      <BgSectionContainer>
+        <Wrapper>
+          <FaqCollection />
+        </Wrapper>
+      </BgSectionContainer>
+      <Image src="/assets/background/faq/lava_cave.png" />
+    </>
   )
 }
