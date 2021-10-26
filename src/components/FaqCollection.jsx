7cf26ef5ef03e7c404391ab40e6672e1aef6affd
Kevin Zou <kevin.zp.zou@gmail.com>
2021-10-25 20:59:17 -0700

    Pull from firebase for FAQ

--- src/components/FaqCollection.jsx
+++ src/components/FaqCollection.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import FaqBox from '@components/FaqBox';
 import { Header1, Header3 } from "@components/Typography";
 import { chunkify } from '@utilities/chunkify';
+import fireDb from '@utilities/firebase';
 
 const Container = styled.div`
   text-align: center;
@@ -26,60 +27,36 @@ const Column = styled.div`
   flex-direction: column;
 `
 
-const faqData = {
-  General: [
-    {
-      title: 'What is hackcamp',
-      description: 'Well it sure as hell aint learn day aha!Well it sure as hell aint learn day aha!Well it sure as hell aint learn day aha!Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp',
-      description: 'Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp',
-      description: 'Well it sure as hell aint learn day aha! Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp???',
-      description: 'Well it sure as hell aint learn day aha! Well it sure as hell aint learn day aha!'
-    },
-  ],
-  "Two Words": [
-    {
-      title: 'What is hackcamp???',
-      description: 'Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp???',
-      description: 'Well it sure as hell aint learn day aha! Well it sure as hell aint learn day aha! Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp???',
-      description: 'Well it sure as hell aint learn day aha! Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp',
-      description: 'Well it sure as hell aint learn day aha!'
-    },
-    {
-      title: 'What is hackcamp',
-      description: 'Well it sure as hell aint learn day aha!'
-    },
-  ],
-}
-
 const singleEntry = (entry) => (
   <FaqBox key={entry.title} title={entry.title}>{entry.description}</FaqBox>
 )
 
 export default function FaqCollection() {
+  const [faqData, setFaqData] = useState(null);
   const [width, setWidth] = useState();
   const mobileBreakpoint = 768;
 
-  useEffect(() => {
+  function processData(data) {
+    const processedData = {};
+    data.forEach(faq => {
+      // Initialize key and value if category hasn't been made yet
+      if (!processedData[`${faq.category}`]) {
+        processedData[`${faq.category}`] = [];
+      }
+      processedData[`${faq.category}`].push({
+        title: faq.question,
+        description: faq.answer,
+      });
+    })
+    return processedData;
+  }
+
+  useEffect(async () => {
     setWidth(window.innerWidth);
     window.addEventListener("resize", () => setWidth(window.innerWidth));
+    const data = await fireDb.getCollection('HackCamp2021', 'FAQ');
+    const processedData = processData(data);
+    setFaqData(processedData);
   }, []);
 
   const COLUMNS_OF_FAQ = width > mobileBreakpoint ? 3 : 2;
@@ -88,14 +65,13 @@ export default function FaqCollection() {
     <Container>
       <Header1 id="faq">Frequently Asked Questions</Header1>
 
-      {
-
+      {faqData &&
         Object.entries(faqData).map(([title, content]) => (
           <>
             <CategoryTitle>{title}</CategoryTitle>
             <Columns>
               {chunkify(content, COLUMNS_OF_FAQ, true).map((chunk) => (
-                <Column key={chunk[0].title}>{chunk.map(singleEntry)}</Column>
+                <Column key={chunk[0]?.title}>{chunk.map(singleEntry)}</Column>
               ))}
             </Columns>
           </>
