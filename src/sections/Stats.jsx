31dc18aa8f28d5fbc7849d9113082b261734e3fe
UBC Student <ansonc14@students.cs.ubc.ca>
2021-11-04 03:50:13 -0700

    add footer and hyperlinks to build day, add milestones title

--- src/sections/Stats.jsx
+++ src/sections/Stats.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useWindowWidth } from '@react-hook/window-size';
 import { SectionContainer } from '@lib/Containers';
 import { SCREEN_BREAKPOINTS } from '../theme/ThemeProvider';
+import { Header1 } from "@components/Typography";
 
 const BgSectionContainer = styled(SectionContainer)`
   background: url('/assets/background/stats/mountains.png') bottom center, linear-gradient(180deg, #55597A 0%, #F36FFF 74.12%);
@@ -18,9 +19,18 @@ const BgSectionContainer = styled(SectionContainer)`
 `
 
 const Image = styled.img`
-  grid-column: 3 / span 10;
   width: 100%;
   z-index: 2;
+`
+
+const Title = styled.div`
+  margin-bottom: 100px;
+  text-align: center;
+`
+
+const Wrapper = styled.div`
+  grid-column: 3 / span 10;
+  margin-bottom: 150px;
 `
 
 export default function Stats() {
@@ -29,12 +39,17 @@ export default function Stats() {
 
   return (
     <BgSectionContainer>
-      {windowWidth &&
-        <Image
-          src={windowWidth > mobileBreakpoint ? '/assets/stats.svg' : '/assets/stats_mobile.svg'}
-          alt="$1200 donated to charity, 528 hackers registered, 48 projects submitted"
-        />
-      }
+      <Wrapper>
+        <Title>
+          <Header1>Milestones</Header1>
+        </Title>
+        {windowWidth &&
+          <Image
+            src={windowWidth > mobileBreakpoint ? '/assets/stats.svg' : '/assets/stats_mobile.svg'}
+            alt="$1200 donated to charity, 528 hackers registered, 48 projects submitted"
+          />
+        }
+      </Wrapper>
     </BgSectionContainer>
   )
 }
