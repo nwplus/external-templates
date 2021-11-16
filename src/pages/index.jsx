05ba6ac1f5c81dec1a6b5a07f52264a64dfce2fd
Anson Chung <anson1014@hotmail.com>
2021-11-16 01:40:02 -0800

    copy typography from hackcamp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import React from 'react'
-import GlobalStyles from '@styles/global'
 // import fireDb from '@utilities/firebase'
 import { SectionContainer } from '@lib/Containers'
+import { Body, Header1, Header2, Header3 } from "@components/Typography";
+import GlobalStyle from "../theme/GlobalStyle";
+
 
 export default function Index({ title }) {
   return (
     <SectionContainer>
-      <GlobalStyles />
+      <GlobalStyle />
+      <div>
+        <Header1>Large Title</Header1>
+        <Header2>Title 1</Header2>
+        <Header3>Title 2</Header3>
+        <Body>Some long lorem ipsum body text that will probably never see the light of day but that is a-ok.</Body>
+      </div>
       <Head>
         {/* Remove comment once title is set */}
         <title> {title} </title>
