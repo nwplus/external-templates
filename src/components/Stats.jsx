a494a39dbfe9263830612819bd26ebbf9159baba
Vincent Chiang <vchiang@amazon.com>
2021-11-24 22:21:26 -0800

    fix mobile

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { Body } from './Typography'
 import { Header2 } from './Typography'
-
-const PC_STATS = 'assets/stats.svg'
+import { useWindowWidth } from '@react-hook/window-size';
+import { SCREEN_BREAKPOINTS } from '../theme/ThemeProvider';
 
 const BgSectionContainer = styled(SectionContainer)`
   background: url('/assets/stats_bg1.svg');
@@ -16,8 +16,10 @@ const BgSectionContainer = styled(SectionContainer)`
   background-position: center center;
   position: relative;
   ${p => p.theme.mediaQueries.mobile} {
-    background-size: auto 100%;
-    min-height: 600px;
+    background: url('/assets/stats_bg1_mobile.svg');
+    background-size: 100vw;
+    background-repeat: no-repeat;
+    height: 103vw;
   }
 `
 const StyledTitle = styled(Header2)`
@@ -26,6 +28,7 @@ const StyledTitle = styled(Header2)`
   left: 45vw;
   
   ${(p) => p.theme.mediaQueries.mobile} {
+    left: 36vw;
     font-size: 2em;
   }
 `
@@ -36,22 +39,17 @@ position: absolute;
   left: 44vw;
   ${(p) => p.theme.mediaQueries.mobile} {
     text-align: center;
+    
   }
 `
 
-const Image = styled.img`
-display: block;
-margin-left: auto;
-margin-right: auto;
-margin-bottom: 250px;
-`
-
 export default function Stats() {
+  const windowWidth = useWindowWidth();
+  const mobileBreakpoint = SCREEN_BREAKPOINTS.mobile;
   return (
     <BgSectionContainer>
       <StyledTitle>Statistics</StyledTitle>
-      <StyledBody>Based on last nwHacks in 2021</StyledBody>
-      {/* <Image src={PC_STATS} alt="nwHacks Stats" /> */}
+      {windowWidth > mobileBreakpoint && <StyledBody>Based on last nwHacks in 2021</StyledBody>}
     </BgSectionContainer>
   )
 }
