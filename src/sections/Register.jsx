71228d375c8195b70f0cbc4d4e15d706be6d5676
derek <derekc150@gmail.com>
2021-11-26 19:39:31 -0800

    white url and 2 line on mobile

--- src/sections/Register.jsx
+++ src/sections/Register.jsx
@@ -70,6 +70,10 @@ const GridContainer = styled.div`
   }
 `
 
+const WhiteURL = styled.a`
+  color: #FFFFFF;
+`
+
 const SponsorLink = styled.a`
     margin-left: 20px;
     transition: 0.25s ease-in-out;
@@ -95,7 +99,8 @@ export default function Register() {
             <BodyContainer>
               <Header3 color='#FFFFFF'>Western Canada’s</Header3>
               <Header3 color='#FFFFFF'>largest hackathon</Header3>
-              <Body color='#FFFFFF'>In-person event @ <u><a target="_blank" rel="noopener noreferrer" href="https://www.google.ca/maps/place/Robert+H.+Lee+Alumni+Centre/@49.2659434,-123.2517296,17z/data=!3m1!4b1!4m5!3m4!1s0x548672b6360dff9d:0x82fa3ec0bf22ed35!8m2!3d49.2659399!4d-123.2495409">UBC Robert H. Lee Alumni Centre</a></u></Body>
+              <Body color='#FFFFFF'>In-person event @</Body>
+              <u><WhiteURL target="_blank" rel="noopener noreferrer" href="https://www.google.ca/maps/place/Robert+H.+Lee+Alumni+Centre/@49.2659434,-123.2517296,17z/data=!3m1!4b1!4m5!3m4!1s0x548672b6360dff9d:0x82fa3ec0bf22ed35!8m2!3d49.2659399!4d-123.2495409">UBC Robert H. Lee Alumni Centre</WhiteURL></u>
             </BodyContainer>
             <MobileButtonContainer>
               <Button target="_blank" rel="noopener noreferrer" href="https://portal.nwplus.io/application" width='259px' height='40px' backgroundColor='linear-gradient(180deg, #FFD12C 0%, #FE800B 100%)' borderRadius='100px' textColor='#224260' borderColor="#FFB72C" isHover isGradientText>
@@ -111,7 +116,7 @@ export default function Register() {
             <Header1 isGradient background='linear-gradient(180deg, #FFD12C 0%, #FE800B 100%)'>nwHacks</Header1>
             <BodyContainer>
               <Header3 color='#FFFFFF'>Western Canada’s largest hackathon</Header3>
-              <Body color='#FFFFFF'>In-person event @ <u><a target="_blank" rel="noopener noreferrer" href="https://www.google.ca/maps/place/Robert+H.+Lee+Alumni+Centre/@49.2659434,-123.2517296,17z/data=!3m1!4b1!4m5!3m4!1s0x548672b6360dff9d:0x82fa3ec0bf22ed35!8m2!3d49.2659399!4d-123.2495409">UBC Robert H. Lee Alumni Centre</a></u></Body>
+              <Body color='#FFFFFF'>In-person event @ <u><WhiteURL target="_blank" rel="noopener noreferrer" href="https://www.google.ca/maps/place/Robert+H.+Lee+Alumni+Centre/@49.2659434,-123.2517296,17z/data=!3m1!4b1!4m5!3m4!1s0x548672b6360dff9d:0x82fa3ec0bf22ed35!8m2!3d49.2659399!4d-123.2495409">UBC Robert H. Lee Alumni Centre</WhiteURL></u></Body>
             </BodyContainer>
             <ButtonContainer>
               <Button target="_blank" rel="noopener noreferrer" href="https://portal.nwplus.io/application" width='137px' height='44px' backgroundColor='linear-gradient(180deg, #FFD12C 0%, #FE800B 100%)' borderRadius='100px' textColor='#224260' borderColor="#FFB72C" isHover isGradientText>
