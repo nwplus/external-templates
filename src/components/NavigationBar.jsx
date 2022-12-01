e8b5531f87ba0b63da16934452a897758f90f3ad
Nicholas Wong <40312631+kozr@users.noreply.github.com>
2022-11-30 20:20:51 -0800

    Add navbar shadow and fix spacing (#275)
    * add navbar shadow and fix spacing
    
    * responsive font and spacing
    
    * bring it closer to figma

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,75 +1,70 @@
-
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
+import { scale } from '@utilities/format'
 
 const NavBarContainer = styled.nav`
   position: fixed;
   top: 0;
   z-index: 999;
   width: 100%;
+  height: 100px;
   display: flex;
   align-items: center;
   justify-content: stretch;
-  visibility: ${(p) => p.visibility};
-  opacity: ${(p) => p.opacity};
+  visibility: ${p => p.visibility};
+  opacity: ${p => p.opacity};
   transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
-  padding: 1.7rem 4rem 0;
+  padding: 0.6rem 4rem 0;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0.1) 49.73%, rgba(89, 88, 90, 0) 100%);
 
-  ${(p) => p.theme.mediaQueries.mobile} {
+  ${p => p.theme.mediaQueries.mobile} {
+    background: none;
     padding: 24px 40px 0;
-    z-index:999;
-    justify-content:flex-end;
+    z-index: 999;
+    justify-content: flex-end;
   }
 `
 
 const NavGroupContainer = styled.div`
   display: flex;
-  gap: 28px;
+  gap: 3%;
   align-items: center;
-  justify-content:space-between;
+  justify-content: space-between;
   width: 100%;
 
-  ${(p) => p.theme.mediaQueries.tablet} {
-    gap: 5px;
-  }
-
-  ${(p) => p.theme.mediaQueries.mobile} {
-    display:none;
+  ${p => p.theme.mediaQueries.mobile} {
+    display: none;
   }
 `
 
 const NavTextContainer = styled.div`
   display: flex;
-  gap: 28px;
+  gap: 4%;
   align-items: center;
   flex-grow: 2;
 
-  ${(p) => p.theme.mediaQueries.tablet} {
-    gap: 15px;
-  }
-
-  ${(p) => p.theme.mediaQueries.mobile} {
+  ${p => p.theme.mediaQueries.mobile} {
     display: none;
   }
 `
 
 const NwPlusLogo = styled.img`
-  max-height:50px;
+  max-height: 50px;
   margin-right: 18px;
 
-  ${(p) => p.theme.mediaQueries.mobile} {
+  ${p => p.theme.mediaQueries.mobile} {
     width: 50px;
     margin-right: 0;
   }
 `
 
 const LinkText = styled.a`
-  color: ${(p) => p.theme.colors.text};
+  color: ${p => p.theme.colors.text};
   text-decoration: none;
-  
+
   ::after {
-    content: "";
+    content: '';
     display: block;
     width: 0;
     height: 2px;
@@ -78,9 +73,9 @@ const LinkText = styled.a`
   }
 
   &:hover {
-    color: ${(p) => p.theme.colors.text};
+    color: ${p => p.theme.colors.text};
     text-decoration: none;
-    
+
     ::after {
       width: 100%;
     }
@@ -88,13 +83,17 @@ const LinkText = styled.a`
 `
 
 const StyledLinkHeaders = styled.h3`
-  font-size: 1.14rem;
-  font-weight:400;
+  font-family: HK Grotesk;
+  font-size: ${() => scale(768, 1440, 16, 18)};
+  font-weight: 600;
+  line-height: 23px;
+  letter-spacing: 0px;
+  text-align: center;
 `
 
 const HamburgerMenu = styled.img`
   display: none;
-  ${(p) => p.theme.mediaQueries.mobile} {
+  ${p => p.theme.mediaQueries.mobile} {
     display: block;
     width: 30px;
   }
@@ -102,20 +101,20 @@ const HamburgerMenu = styled.img`
 
 const DropDownContentContainer = styled.div`
   position: fixed;
-  top:0;
+  top: 0;
   z-index: 998;
   padding: 30px 40px 24px 40px;
-  display: flex;  
+  display: flex;
   flex-direction: column;
   align-items: center;
   gap: 24px;
   width: 100%;
-  background: #DBCEE3;
+  background: #dbcee3;
 `
 
 const PortalButtonContainer = styled.div`
-  visibility: ${(p) => p.portalOpen !== null ? 'visible' : 'hidden'};
-  opacity: ${(p) => p.portalOpen !== null ? '1' : '0'};
+  visibility: ${p => (p.portalOpen !== null ? 'visible' : 'hidden')};
+  opacity: ${p => (p.portalOpen !== null ? '1' : '0')};
   transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
   justify-self: flex-end;
   user-select: none;
@@ -128,21 +127,21 @@ const StyledPortalText = styled.div`
 const Button = styled.a`
   display: table;
   text-decoration: none;
-  position:relative;
-  padding:11px 21px;
-  border-radius:50px;
+  position: relative;
+  padding: 11px 21px;
+  border-radius: 50px;
   font-weight: bold;
-  background: linear-gradient(to right, #0DEFE1, #78FF96);
-  color: #2C2543;
+  background: linear-gradient(to right, #0defe1, #78ff96);
+  color: #2c2543;
 
   &::before {
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    content: "Live Portal";
-    color:#2C2543;
-    
-    border-radius:50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    content: 'Live Portal';
+    color: #2c2543;
+
+    border-radius: 50px;
     position: absolute;
     top: 0;
     right: 0;
@@ -152,21 +151,25 @@ const Button = styled.a`
     z-index: 1;
     transition: opacity 0.25s ease-in-out;
     opacity: 0;
-    
-    background: linear-gradient(to right, #00DBCE, #00D88A);
+
+    background: linear-gradient(to right, #00dbce, #00d88a);
   }
 
   &:hover::before {
     opacity: 1;
   }
 
-  ${p => !p.disabled && `
+  ${p =>
+    !p.disabled &&
+    `
     &:hover {
       cursor:pointer;
     }
   `}
 
-  ${p => p.disabled && `
+  ${p =>
+    p.disabled &&
+    `
     display: none;
     cursor: default;
 
@@ -185,7 +188,7 @@ const MenuItem = ({ name, href, isAnchor, target, rel, isMobile, closeDropdown }
     }
   }, [href])
 
-  const handleClick = (event) => {
+  const handleClick = event => {
     if (isAnchor && anchorTarget) {
       event.preventDefault()
       anchorTarget.scrollIntoView({ behavior: 'smooth', block: 'start' })
@@ -205,28 +208,26 @@ const MenuItem = ({ name, href, isAnchor, target, rel, isMobile, closeDropdown }
 const PortalButton = ({ portalOpen }) => (
   <PortalButtonContainer portalOpen={portalOpen}>
     <Button
-      width='130px'
-      height='45px'
-      borderRadius='100px'
+      width="130px"
+      height="45px"
+      borderRadius="100px"
       isGradient
-      textColor='black'
-      href='https://portal.nwplus.io'
-      target='_blank'
+      textColor="black"
+      href="https://portal.nwplus.io"
+      target="_blank"
       disabled={!portalOpen}
     >
-      <StyledPortalText disabled={!portalOpen}>
-        Live Portal
-      </StyledPortalText>
+      <StyledPortalText disabled={!portalOpen}>Live Portal</StyledPortalText>
     </Button>
   </PortalButtonContainer>
 )
 
 const MenuList = ({ isMobile, closeDropdown }) => (
   <>
-    <MenuItem name='About' href='/#about' isAnchor isMobile={isMobile} closeDropdown={closeDropdown} />
+    <MenuItem name="About" href="/#about" isAnchor isMobile={isMobile} closeDropdown={closeDropdown} />
     {/* <MenuItem name='Statistics' href='/#statistics' isAnchor isMobile={isMobile} closeDropdown={closeDropdown} /> */}
-    <MenuItem name='FAQ' href='/#faq' isAnchor isMobile={isMobile} closeDropdown={closeDropdown} />
-    <MenuItem name='Sponsors' href='/#sponsors' isAnchor isMobile={isMobile} closeDropdown={closeDropdown} />
+    <MenuItem name="FAQ" href="/#faq" isAnchor isMobile={isMobile} closeDropdown={closeDropdown} />
+    <MenuItem name="Sponsors" href="/#sponsors" isAnchor isMobile={isMobile} closeDropdown={closeDropdown} />
     {/* <MenuItem name='2022' href='https://2022.nwhacks.io' target='_blank' rel='noopener' isMobile={isMobile} closeDropdown={closeDropdown} /> */}
   </>
 )
@@ -276,17 +277,15 @@ const NavigationBar = () => {
       <>
         <NavBarContainer mobileView>
           <HamburgerMenu
-            src='/images/icons/cross.svg'
-            alt='dropdown menu icon'
+            src="/images/icons/cross.svg"
+            alt="dropdown menu icon"
             onClick={() => setShowDropdown(false)}
           />
         </NavBarContainer>
 
         <DropDownContentContainer>
-          <a href='/'>
-            <NwPlusLogo
-              src='/images/logos/nwplus-logo.svg'
-              alt='nwPlus club logo in white' />
+          <a href="/">
+            <NwPlusLogo src="/images/logos/nwplus-logo.svg" alt="nwPlus club logo in white" />
           </a>
           <MenuList isMobile={showDropdown} closeDropdown={setShowDropdown} />
           <PortalButton portalOpen={false} />
@@ -301,10 +300,8 @@ const NavigationBar = () => {
   return (
     <NavBarContainer visibility={visibility} opacity={opacity}>
       <NavGroupContainer>
-        <a href='/'>
-          <NwPlusLogo
-            src='/images/logos/nwplus-logo.svg'
-            alt='nwPlus club logo in white' />
+        <a href="/">
+          <NwPlusLogo src="/images/logos/nwplus-logo.svg" alt="nwPlus club logo in white" />
         </a>
         <NavTextContainer>
           <MenuList />
@@ -312,13 +309,9 @@ const NavigationBar = () => {
         <PortalButton portalOpen={false} />
       </NavGroupContainer>
 
-      <HamburgerMenu
-        src='/images/icons/menu.svg'
-        alt='dropdown menu icon'
-        onClick={() => setShowDropdown(true)} />
+      <HamburgerMenu src="/images/icons/menu.svg" alt="dropdown menu icon" onClick={() => setShowDropdown(true)} />
 
       {/* <TrustBadge /> */}
-
     </NavBarContainer>
   )
 }
