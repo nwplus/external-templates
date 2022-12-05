f9fca27febee8bb67ea80a17096273489bf703d2
kozr <zxzflow@gmail.com>
2022-12-05 01:27:46 -0800

    tier locations

--- src/components/SponsorsGrid.jsx
+++ src/components/SponsorsGrid.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -7,8 +7,8 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   padding-bottom: 40vw;
-  
-  ${(p) => p.theme.mediaQueries.mobile} {
+
+  ${p => p.theme.mediaQueries.mobile} {
     padding-top: 2rem;
     min-height: 100vh;
   }
@@ -22,9 +22,8 @@ const Grid = styled.div`
   grid-template-columns: repeat(3, 1fr);
   padding-top: 3rem;
   gap: 2rem;
-  
-  
-  ${(p) => p.theme.mediaQueries.mobile} {
+
+  ${p => p.theme.mediaQueries.mobile} {
     display: flex;
     flex-direction: column;
     min-width: 0;
@@ -37,36 +36,64 @@ const SponsorImg = styled.img`
   width: 100%;
   border: none;
   object-fit: contain;
-  ${(p) => p.theme.mediaQueries.mobile} {
-    
+  ${p => p.theme.mediaQueries.mobile} {
   }
 `
 
 const SponsorsGrid = ({ sponsors }) => {
+  const emptyTierList = { platinum: [], gold: [], silver: [], bronze: [], Inkind: [] }
+  const [tierList, setTierList] = useState(emptyTierList)
+
+  useEffect(() => {
+    if (sponsors) {
+      const updatedTierList = emptyTierList
+      sponsors.forEach(sponsor => {
+        updatedTierList[sponsor.tier].push(sponsor)
+      })
+      setTierList(updatedTierList)
+    }
+  }, [sponsors])
+
   const Sponsor = ({ link, url }) => (
-    <a href={link} target="_blank" rel="noreferrer" style={{
-      width: '100%',
-      aspectRatio: '1/1',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      style={{
+        width: '100%',
+        aspectRatio: '1/1',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <SponsorImg src={url} />
     </a>
+  )
+
+  const ListByTier = ({ listOfSponsors }) => (
+    <>
+      {listOfSponsors?.map(item => (
+        <Sponsor
+          {...{
+            key: item.name,
+            link: item.link,
+            url: item.imgURL,
+          }}
+        />
+      ))}
+      <br />
+    </>
   )
 
   return (
     <Container>
       <Grid>
-        {sponsors?.length > 0
-          ? sponsors.map(item => (
-            <Sponsor {...{
-              key: item.name,
-              link: item.link,
-              url: item.imgURL
-            }} />
-          ))
-          : ''}
+        <ListByTier listOfSponsors={tierList.platinum} />
+        <ListByTier listOfSponsors={tierList.gold} />
+        <ListByTier listOfSponsors={tierList.silver} />
+        <ListByTier listOfSponsors={tierList.bronze} />
+        <ListByTier listOfSponsors={tierList.Inkind} />
       </Grid>
     </Container>
   )
