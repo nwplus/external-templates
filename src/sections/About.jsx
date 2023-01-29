f853ee2eea9f6ac820687e406a9e00104878391f
DonaldKLee <Donald3010@outlook.com>
2023-01-29 15:56:46 -0800

    Added About Section background

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
+import AboutBackground from "../../public/images/backgrounds/About.svg"
+
+
 const AboutContainer = styled.div`
   position: relative;
   z-index: 1;
@@ -20,8 +24,21 @@ const AboutContainer = styled.div`
     // aspect-ratio: 428 / 1344;
   }
 `
+
+const AboutBg = styled.img`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: auto;
+  // Makes background non-selectable
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+`
+
 const About = () => (
   <AboutContainer>
+    <AboutBg src={AboutBackground} />
     About
   </AboutContainer>
 )
