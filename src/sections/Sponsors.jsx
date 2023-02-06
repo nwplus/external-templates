3225f89258aca1e82430f0f68ed4058490545ce5
Amy Jo <amyjojjm2001@gmail.com>
2023-02-06 15:35:58 -0800

    sponsor text

--- src/sections/Sponsors.jsx
+++ src/sections/Sponsors.jsx
@@ -1,5 +1,33 @@
 import SVG from 'react-inlinesvg'
+import styled from 'styled-components'
 
-const Sponsors = () => <SVG src="/assets/Sponsors + Footer.svg" />
+const SponsorText = styled.div`
+  position: absolute;
+  top: 9.5%;
+  color: white;
+  font-family: 'HK Grotesk';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  left: 50%;
+  transform: translateX(-50%);
+`
+
+const Sponsors = () => {
+  return (
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <SponsorText>
+        <p style={{ display: 'inline' }}>
+          nwPlus is always looking for new ventures, opportunities, and connections. If you are interested in sponsoring
+          us, working with us, or speaking at one of our events, shoot us an email at&nbsp;
+        </p>
+        <a style={{ display: 'inline', textDecoration: 'underline', cursor: 'pointer' }}>sponsorship@nwplus.io.</a>
+      </SponsorText>
+
+      <SVG src="/assets/Sponsors + Footer.svg" />
+    </div>
+  )
+}
 
 export default Sponsors
