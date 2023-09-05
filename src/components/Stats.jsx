9180b04f34ab81bd5949680480c334fee3c735dd
alvinkam <alvin.kam.33@gmail.com>
2023-09-04 21:27:15 -0700

    stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,25 +1,28 @@
-import 'animate.css/animate.min.css'
-// import { useParallax } from 'react-scroll-parallax';
+import { useParallax } from 'react-scroll-parallax'
 import { SectionContainer } from '@lib/Containers'
-import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import { Header2 } from './Typography'
-import { AnimationOnScroll } from 'react-animation-on-scroll'
 
-// import { useWindowWidth } from '@react-hook/window-size';
-// import { Body, Header2 } from './Typography'
-// import { SCREEN_BREAKPOINTS } from '../theme/ThemeProvider';
+const gallery = [
+  '/assets/gallery/1.png',
+  '/assets/gallery/2.png',
+  '/assets/gallery/3.png',
+  '/assets/gallery/4.png',
+  '/assets/gallery/5.png',
+  '/assets/gallery/6.png',
+  '/assets/gallery/7.png'
+]
 
 const BgSectionContainer = styled(SectionContainer)`
-  background: linear-gradient(to bottom, #ADDBF3, #F2B491);
+  background: #150C27;
   display: grid;
   align-items: center;
   width: 100%;
-  aspect-ratio: 1440/840;
+  aspect-ratio: 1440/1415;
   position: relative;
 
   z-index: 15;
-  overflow-y: hidden;
+  overflow: hidden;
 
   ${p => p.theme.mediaQueries.mobile} {
     background: url('assets/mobile/stats/static.svg');
@@ -32,7 +35,7 @@ const BgScroll = styled(SectionContainer)`
   background: url('assets/background/stats/background.svg');
   background-size: 100vw;
   background-repeat: no-repeat;
-  background-position: center center;
+  background-position: center top;
 
   position: absolute;
   top: 0;
@@ -45,15 +48,16 @@ const BgScroll = styled(SectionContainer)`
     background: none;
   }
 `
-const MgScroll = styled(SectionContainer)`
-  background: url('assets/background/stats/midground.svg');
+
+const Stems = styled(SectionContainer)`
+  background: url('assets/background/stats/stems.svg');
   background-size: 100vw;
+  transform: scale(1.205);
   background-repeat: no-repeat;
-  background-position: center center;
 
   position: absolute;
-  top: 0;
-  left: 0;
+  top: 46.5vw;
+  left: -0.5vw;
   width: 100%;
   height: 100%;
   z-index: 12;
@@ -65,60 +69,17 @@ const MgScroll = styled(SectionContainer)`
 
 `
 
-const bop = keyframes`
-  0% {
-    transform: translate(0, -0%);
-  }
-  50% {
-    transform: translate(0, -2%);
-  }
-  100% {
-    transform: translate(0, -0%);
-  }
-`
-
-const Crane = styled.img`
-  display: none;
-
-  ${(p) => p.theme.mediaQueries.mobile} {
-    display: table;
-    
-    width: 100%;
-    object-fit: contain;
-    margin: 0;
-    position: relative;
-    animation-name: ${bop};
-    animation-duration: 8s;
-    animation-iteration-count: infinite;
-  }
-`
-
-const bopdesktop = keyframes`
-  0% {
-    transform: translate(0, -0%);
-  }
-  50% {
-    transform: translate(0, -10%);
-  }
-  100% {
-    transform: translate(0, -0%);
-  }
-`
-
-const Flying = styled(SectionContainer)`
-  background: url('assets/background/stats/foreground.svg'); // flying dino holding stats
+const FgScroll = styled(SectionContainer)`
+  background: url('assets/background/stats/foreground.svg');
   background-size: 100vw;
   background-repeat: no-repeat;
-  background-position: center center;
-  width: 100vw;
-  aspect-ratio: 1479 / 400;
-  margin: 5rem 0;
-  z-index: 15;
-  position: relative;
-  animation-name: ${bopdesktop};
-  animation-duration: 4s;
-  animation-iteration-count: infinite;
-  margin-left: 50px;
+  background-position: center bottom;
+
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 98;
+
   ${(p) => p.theme.mediaQueries.mobile} {
     display: none;
   }
@@ -126,18 +87,20 @@ const Flying = styled(SectionContainer)`
 
 const ContentInner = styled.div`
   display: flex;
+  margin-bottom: auto;
   flex-direction: column;
-  justify-content: center;
+
   width: 100%;
-  align-items: center;
+  height: 100%;
+  padding-top: 4.5rem;
   overflow-x: hidden;
+  z-index: 99;
 `
 
 const StyledTitle = styled(Header2)`
   text-align: center;
-  color: #002F4D;
-  font-size: 3rem;
-  padding-top: 5rem;
+  color: #FFFAFA;
+  font-size: 3.75vw;
 
   ${(p) => p.theme.mediaQueries.mobile} {
     font-size: 2em;
@@ -145,10 +108,65 @@ const StyledTitle = styled(Header2)`
   }
 `
 
-export default function Stats() {
-  // const windowWidth = useWindowWidth();
-  // const mobileBreakpoint = SCREEN_BREAKPOINTS.mobile;
+const StatsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 4rem 40% 0;
+  gap: 2rem;
+`
 
+const StatTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #FFFFFF;
+  font-size: 1.5vw;
+  font-weight: 700;
+  word-spacing: 200px;
+  max-width: 200px;
+  line-height: 33px;
+`
+
+const StatBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #FFD7D9;
+  font-size: 1.5vw;
+  font-weight: 700;
+  word-spacing: 200px;
+  max-width: 200px;
+  line-height: 33px;
+`
+
+const Number = styled.div`
+  font-size: 3.75vw;
+  font-weight: 900;
+  margin-bottom: 0.5rem;
+`
+
+const scroll = keyframes`
+  0% { 
+    transform: translateX(0); 
+  }
+  100% { 
+    transform: translateX(-2568.38px); 
+  }
+`
+// animation translation matches full width of images + gap
+
+const Gallery = styled.div`
+  background: #150C27;
+  animation: ${scroll} 40s linear infinite;
+  display: flex;
+  position: absolute;
+  top: 73.5%;
+  z-index: 97;
+`
+
+const GalleryImage = styled.img`
+  margin: 10px 15px;
+`
+
+export default function Stats() {
   // const { ref: ref1 } = useParallax({
   //   speed: -20,
   // });
@@ -160,19 +178,45 @@ export default function Stats() {
   return (
     <BgSectionContainer id="statistics">
       <BgScroll />
-      <MgScroll />
-      <Crane src="assets/mobile/stats/flying.svg" alt="Flying doode" />
-
+      <Stems />
+      <FgScroll />
       <ContentInner>
         <StyledTitle>
-          Statistics
+          Last year we had:
+          <StatsGrid>
+            <StatTop>
+              <Number>26</Number>
+              Projects Submitted
+            </StatTop>
+            <StatTop>
+              <Number>197</Number>
+              Hackers Registered
+            </StatTop>
+            <StatBottom>
+              <Number>40%</Number>
+              Gender Minority
+            </StatBottom>
+            <StatBottom>
+              <Number>64%</Number>
+              First-Time Hackers
+            </StatBottom>
+          </StatsGrid>
         </StyledTitle>
-
-        <AnimationOnScroll style={{ zIndex: 15 }} animateIn="animate__fadeInRightBig">
-          <Flying />
-        </AnimationOnScroll>
-
       </ContentInner>
+      <Gallery>
+        {gallery.map((img) => (
+          <GalleryImage
+            key={img}
+            src={img}
+          />
+        ))}
+        {gallery.map((img) => (
+          <GalleryImage
+            key={`${img}2`}
+            src={img}
+          />
+        ))}
+      </Gallery>
     </BgSectionContainer>
   )
 }
