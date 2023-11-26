3d679fa3dc3a78f3ffc0b9d27b01422a12a35ca7
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2023-11-26 14:26:18 +0000

    made 'what can I make' section

--- src/sections/Tracks.jsx
+++ src/sections/Tracks.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Body, Header2 } from '@components/Typography'
+import Healthcare from '@assets/images/card_healthcare.svg'
+import Community from '@assets/images/card_community.svg'
+import Sustainability from '@assets/images/card_sustainability.svg'
+import Wildcard from '@assets/images/card_wildcard.svg'
 
 const TrackContainer = styled.div`
   position: relative;
@@ -23,7 +27,8 @@ const TrackInner = styled.div`
   max-width: 1200px;
   margin: 0 auto;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
   gap: 5rem;
   padding-bottom: 5rem;
 
@@ -34,7 +39,7 @@ const TrackInner = styled.div`
   }
 `
 
-const BlurbContainer = styled.div`
+const TextElements = styled.div`
   display: flex;
   flex-direction: column;
   width: 60%;
@@ -55,33 +60,108 @@ const BlurbContainer = styled.div`
   }
 `
 
-const BlurbHeader = styled(Header2)`
-  color: #F3F5F4;
+const Header = styled(Header2)`
+  color: #f3f5f4;
 
   ${p => p.theme.mediaQueries.mobile} {
     font-size: 2rem;
   }
 `
 
-const BlurbText = styled(Body)`
+const Description = styled(Body)`
   padding-top: 2rem;
   font-weight: normal;
   font-size: 1.1rem;
   line-height: 150%;
-  color: #F3F5F4;
+  color: #f3f5f4;
 `
 
+const Instruction = styled.h3`
+  padding-top: 8rem;
+  font-weight: bold;
+  font-size: 1.5rem;
+  line-height: 150%;
+  color: #f3f5f4;
+`
+
+const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 3rem;
+  width: fit-content;
+`
+
+const CardImg = styled.img`
+  box-shadow: #0f212c -12px 12px;
+  border-radius: 15px;
+  transition: 300ms;
+`
+
+const LOREM =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac justo consectetur ex cursus sagittis. Nam sit amet mi quis ipsum volutpat accumsan nec vitae nulla. Etiam lobortis tempus cursus. Nunc eu sem vitae risus porta iaculis sit amet eget ante. Fusce sed sem sit amet leo sagittis vehicula. Sed ac est aliquet, pharetra ligula vel, egestas orci. Aliquam aliquam arcu eget tristique pellentesque. Nunc nec risus non leo ullamcorper finibus. Aliquam imperdiet ac risus a auctor. Curabitur vehicula eu augue consectetur varius. Aenean ut ultricies velit. Mauris id elementum quam, vel lobortis urna.'
+
+const CONTENT = {
+  default: {
+    heading: 'What can I make at nwHacks?',
+    description: (
+      <Description>{`This year, we’re introducing nwTracks! We’ll be rewarding the top projects in each category. You can submit to just one of the tracks - choose wisely!`}</Description>
+    ),
+  },
+  healthcare: {
+    heading: 'Healthcare & Accessibility',
+    description: <Description>{LOREM}</Description>,
+    card: Healthcare,
+  },
+  sustainability: {
+    heading: 'Sustainability & Environment',
+    description: <Description>{LOREM}</Description>,
+    card: Sustainability,
+  },
+  community: {
+    heading: 'Community & Connection',
+    description: <Description>{LOREM}</Description>,
+    card: Community,
+  },
+  wildcard: {
+    heading: 'Wildcard',
+    description: <Description>{LOREM}</Description>,
+    card: Wildcard,
+  },
+}
+
 const Track = () => {
+  const [selection, setSelection] = useState('default')
+
+  const getProps = key => ({
+    src: CONTENT[key].card,
+    alt: CONTENT[key].heading,
+    onMouseOver: () => {
+      setSelection(key)
+    },
+    style: { filter: !['default', key].includes(selection) ? 'grayscale(1)' : undefined },
+  })
 
   return (
     <TrackContainer>
       <TrackInner>
-        <BlurbContainer bias="left">
-          <BlurbHeader>Lincoln's task :)</BlurbHeader>
-          <BlurbText>
-            You got this!!
-          </BlurbText>
-        </BlurbContainer>
+        <TextElements bias="left">
+          <Header>{CONTENT[selection].heading}</Header>
+          {CONTENT[selection].description}
+
+          {selection === 'default' && <Instruction>▶ Hover over each card to learn more!</Instruction>}
+        </TextElements>
+
+        <CardGrid
+          onMouseLeave={() => {
+            setSelection('default')
+          }}
+        >
+          <CardImg {...getProps('healthcare')} />
+          <CardImg {...getProps('sustainability')} />
+          <CardImg {...getProps('community')} />
+          <CardImg {...getProps('wildcard')} />
+        </CardGrid>
       </TrackInner>
     </TrackContainer>
   )
