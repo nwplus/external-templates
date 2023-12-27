e244d118617659f267de0e110619b6800787024d
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2023-12-27 11:27:00 +0000

    pitstop commit

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
-import LoadingScreenImg from 'src/assets/images/loadingScreen.png'
+// import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
 
 const LoadingScreenContainer = styled.div`
   position: fixed;
@@ -11,11 +10,33 @@ const LoadingScreenContainer = styled.div`
   height: 100%;
   background-color: #142e3d;
 `
+const TitleContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+  padding: 2rem;
+  background-color: #70605a;
+  border-bottom: solid #433f3f 0.3rem;
+  box-shadow: -22.94px 18.53px 193.2px 0px #fff8cc80;
+`
+
+const TitleContent = styled.div`
+  border: solid #433f3f 0.3rem;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`
 
 const LoadingScreen = () => {
-  const hello = 10
-
-  return <LoadingScreenContainer></LoadingScreenContainer>
+  return (
+    <LoadingScreenContainer>
+      <TitleContainer>
+        <TitleContent>nwHacks</TitleContent>
+      </TitleContainer>
+    </LoadingScreenContainer>
+  )
 }
 
 export default LoadingScreen
