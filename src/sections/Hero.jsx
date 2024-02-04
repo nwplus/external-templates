d05fc8ac4a7bcc4859caf194ddd5fb60210fa03d
Yan Sidyakin <sidyakinian@gmail.com>
2024-02-04 11:19:11 -0500

    Style hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,139 +7,143 @@ import styled from 'styled-components'
 
 import CmdFLogoImage from '../../public/assets/cmd-f-logo.svg'
 
-const HeroContainer = styled.div`
+const Logo = styled.img`
   position: absolute;
-  left: 50vh;
-  top: 20vh;
+  top: 21.6vh;
+  left: 47.6vh;
+  width: 17.03vh;
+  height: 14.17vh;
 `
 
-const LargeTitleTop = styled.h1`
-  font-family: 'Yatra One', sans-serif;
-  font-size: 40px;
-  font-weight: normal;
-  line-height: 120%;
-  color: #2e2e2e;
+const MainTitle = styled.h1`
+  position: absolute;
+  top: 37.5vh;
+  left: 47.12vh;
+  width: 100.06vh;
+
+  color: #2E2E2E;
+  font-feature-settings: 'liga' off;
+  font-family: "Yatra One";
+  font-size: 4.28vh;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 5.14vh; /* 50.4px */
+  letter-spacing: 0.38px;
 `
 
-const LargeTitleBottom = styled.h1`
-  font-family: 'Yatra One', sans-serif;
-  font-size: 40px;
-  font-weight: normal;
-  line-height: 120%;
-  color: #B23C3C;
-`
+const InfoTitle = styled.p`
+  position: absolute;
+  top: 49.36vh;
+  left: 47.12vh;
 
-const DateTitle = styled.p`
-  margin-top: 12px;
   color: #2E2E2E;
   font-feature-settings: 'liga' off;
 
-  font-size: 28px;
+  font-family: "HK Grotesk";
+  font-size: 3.26vh;
   font-style: normal;
   font-weight: 600;
-  line-height: 32px; /* 100% */
+  line-height: 3.26vh;
   letter-spacing: -0.26px;
 `
 
-const ApplyTitle = styled.p`
-  margin-top: 20px;
+const ApplyLabel = styled.p`
+  position: absolute;
+  top: 56.71vh;
+  left: 47.12vh;
+
   color: #2E2E2E;
   font-feature-settings: 'liga' off;
-
-  font-size: 28px;
+  font-family: "HK Grotesk";
+  font-size: 3.26vh;
   font-style: normal;
   font-weight: 700;
-  line-height: 32px; /* 100% */
+  line-height: 3.26vh;
   letter-spacing: -0.26px;
 `
 
 const ApplyButtonsContainer = styled.div`
+  position: absolute;
+  top: 62.8vh;
+  left: 47.12vh;
+
   display: inline-flex;
   align-items: flex-end;
-  gap: 17px;
-  margin-top: 20px;
+  gap: 1.73vh;
 `
 
 const HackerButton = styled.button`
   display: flex;
-  padding: 18px 28px;
+  padding: 1.83vh 2.85vh;
   justify-content: center;
   align-items: center;
-  gap: 5px;
+  gap: 0.51vh;
   border: none;
-  border-radius: 11.7px;
+  border-radius: 1.19vh;
   background: linear-gradient(90deg, #CC5A5A 15.65%, #DB7859 94.27%);
 
   color: #FFF;
   font-feature-settings: 'liga' off;
   font-family: "HK Grotesk";
-  font-size: 24px;
+  font-size: 2.44vh;
   font-style: normal;
   font-weight: 700;
-  line-height: 26px; /* 108.333% */
+  line-height: 2.65vh;
   letter-spacing: -0.45px;
 `
 
 const MentorButton = styled.button`
   display: flex;
-  padding: 18px 29.5px;
+  padding: 1.83vh 3vh;
   justify-content: center;
   align-items: center;
-  gap: 5px;
+  gap: 0.51vh;
   border: none;
-  border-radius: 11.7px;
+  border-radius: 1.19vh;
   background: linear-gradient(90deg, #E48D4D 16.91%, #E9B145 100%);
 
   color: #FFF;
   font-feature-settings: 'liga' off;
   font-family: "HK Grotesk";
-  font-size: 24px;
+  font-size: 2.44vh;
   font-style: normal;
   font-weight: 700;
-  line-height: 26px; /* 108.333% */
+  line-height: 2.65vh;
   letter-spacing: -0.45px;
 `
 
 const VolunteerButton = styled.button`
   display: flex;
-  padding: 18px 21px;
+  padding: 1.83vh 2.14vh;
   justify-content: center;
   align-items: center;
-  gap: 5px;
+  gap: 0.51vh;
   border: none;
-  border-radius: 11.7px;
+  border-radius: 1.19vh;
   background: linear-gradient(90deg, #49AAB3 9.37%, #67C2B7 100%);
 
   color: #FFF;
   font-feature-settings: 'liga' off;
   font-family: "HK Grotesk";
-  font-size: 24px;
+  font-size: 2.44vh;
   font-style: normal;
   font-weight: 700;
-  line-height: 26px; /* 108.333% */
+  line-height: 2.65vh;
   letter-spacing: -0.45px;
 `
 
-const Logo = styled.img`
-  width: 120px;
-  height: 100px;
-`
-
 const Hero = () => {
-  return (<div id="hero">
-    <HeroContainer>
-      <Logo src={CmdFLogoImage}/>
-      <LargeTitleTop>Western Canada's largest hackathon</LargeTitleTop>
-      <LargeTitleBottom>celebrating underrepresented genders in tech</LargeTitleBottom>
-      <DateTitle>March 9-10, 2024 | In-person (location TBD)</DateTitle>
-      <ApplyTitle>Apply to be a:</ApplyTitle>
-      <ApplyButtonsContainer>
-        <HackerButton>Hacker</HackerButton>
-        <MentorButton>Mentor</MentorButton>
-        <VolunteerButton>Volunteer</VolunteerButton>
-      </ApplyButtonsContainer>
-    </HeroContainer>
-  </div>)
+  return <div id="hero">
+    <Logo src={CmdFLogoImage}/>
+    <MainTitle>Western Canada's largest hackathon <span style={{ color: '#B23C3C' }}>celebrating underrepresented genders in tech</span></MainTitle>
+    <InfoTitle>March 9-10, 2024 | In-person (location TBD)</InfoTitle>
+    <ApplyLabel>Apply to be a:</ApplyLabel>
+    <ApplyButtonsContainer>
+      <HackerButton>Hacker</HackerButton>
+      <MentorButton>Mentor</MentorButton>
+      <VolunteerButton>Volunteer</VolunteerButton>
+    </ApplyButtonsContainer>
+  </div>
 }
 
 export default Hero
