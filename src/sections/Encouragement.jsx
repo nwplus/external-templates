e14c2665ffa4d0c48183bd5d734ef729c57392fe
Yan Sidyakin <sidyakinian@gmail.com>
2024-02-04 11:26:06 -0500

    Style encouragement section

--- src/sections/Encouragement.jsx
+++ src/sections/Encouragement.jsx
@@ -1,40 +1,39 @@
 import styled from 'styled-components';
 
-const EncouragementSection = styled.div`
-  position: absolute;
-  left: 145vh;
-  top: 20vh;
-  width: 564px;
-  text-align: left;
-`;
-
 const Title = styled.h3`
+  position: absolute;
+  left: 160.95vh;
+  top: 16.62vh;
+  width: 57.52vh;
+
   color: #08363C;
   font-family: "Yatra One";
-  font-size: 56px;
+  font-size: 5.71vh;
   font-style: normal;
   font-weight: 400;
-  line-height: 56px; /* 100% */
+  line-height: 5.71vh;
   letter-spacing: 0.4px;
 `;
 
 const Text = styled.p`
-  margin-top: 20px;
+  position: absolute;
+  left: 160.95vh;
+  top: 36.92vh;
+  width: 56.2vh;
+
   color: #08363C;
-  font-size: 20px;
+  font-family: "HK Grotesk";
+  font-size: 2.04vh;
   font-style: normal;
   font-weight: 500;
-  line-height: 22px; /* 110% */
+  line-height: 2.24vh;
   letter-spacing: -0.1px;
 `;
 
 const Encouragement = () => (
     <>
-      <EncouragementSection>
-        <Title>Ahoy hackers!</Title>
-        <Title>Embark on a tech adventure at cmd-f!</Title>
-        <Text>Learn new skills, build with passion and connect with a community dedicated to making a difference. No matter your background or technical expertise, we provide the resources and support to ensure your journey is a success.</Text>
-      </EncouragementSection>
+      <Title>Ahoy hackers! Embark on a tech adventure at cmd-f!</Title>
+      <Text>Learn new skills, build with passion and connect with a community dedicated to making a difference. No matter your background or technical expertise, we provide the resources and support to ensure your journey is a success.</Text>
     </>
 );
 
