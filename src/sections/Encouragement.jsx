29d435bdabb567ec2ec42009c706fca0b93d54af
Yan Sidyakin <sidyakinian@gmail.com>
2024-02-08 23:11:53 -0500

    Adjust spacing for encouragement

--- src/sections/Encouragement.jsx
+++ src/sections/Encouragement.jsx
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
 
-const Title = styled.h3`
-  position: absolute;
+const Container = styled.div`
+  position: relative;
   left: 154.7vh;
   top: 15.98vh;
+`
+
+const Title = styled.h3`
+  left: 0;
+  top: 0;
   width: 55.83vh;
 
   color: #08363C;
   font-family: "Yatra One";
-  font-size: 5.49vh;
+  font-size: calc(1rem + 3vh);
+  // font-size: 3rem;
   font-style: normal;
   font-weight: 400;
   line-height: 100%;
@@ -16,14 +22,13 @@ const Title = styled.h3`
 `;
 
 const Text = styled.p`
-  position: absolute;
-  left: 154.7vh;
-  top: 35.49vh;
+  margin-top: 1.5rem;
+  left: 0;
   width: 54.01vh;
 
   color: #08363C;
   font-family: "HK Grotesk Medium";
-  font-size: 1.96vh;
+  font-size: calc(0.5rem + 1.5vh);
   font-style: normal;
   font-weight: 500;
   line-height: 110%;
@@ -32,8 +37,11 @@ const Text = styled.p`
 
 const Encouragement = () => (
     <>
-      <Title>Ahoy hackers! Embark on a tech adventure at cmd-f!</Title>
-      <Text>Learn new skills, build with passion and connect with a community dedicated to making a difference. No matter your background or technical expertise, we provide the resources and support to ensure your journey is a success.</Text>
+      <Container>
+        <Title>Ahoy hackers!</Title>
+        <Title style={{ marginTop: "1rem" }}>Embark on a tech adventure at cmd-f!</Title>
+        <Text>Learn new skills, build with passion and connect with a community dedicated to making a difference. No matter your background or technical expertise, we provide the resources and support to ensure your journey is a success.</Text>
+      </Container>
     </>
 );
 
