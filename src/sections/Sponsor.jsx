e650bf28a3d8dbd17276fc60bdbe0af3b0dfcd8f
byronwang93 <byronwang123919@gmail.com>
2024-09-02 19:14:57 -0700

    sponsor section completed - web version

--- src/sections/Sponsor.jsx
+++ src/sections/Sponsor.jsx
@@ -17,7 +17,7 @@ const sponsorTierOrder = {
 
 const BgSectionContainer = styled(SectionContainer)`
   /* background: url('assets/background/sponsors/background.png'), #150C27; */
-  background-size: 100vw;
+  background-size: 20vw;
   background-repeat: no-repeat;
   background-position: center top;
   position: relative;
@@ -29,7 +29,6 @@ const BgSectionContainer = styled(SectionContainer)`
   z-index: 99;
 
   ${p => p.theme.mediaQueries.mobile} {
-    /* background: url('assets/mobile/sponsors/background.svg') #150C27; */
     background-size: 100vw;
     background-repeat: no-repeat;
     background-position: center center;
@@ -37,13 +36,17 @@ const BgSectionContainer = styled(SectionContainer)`
   }
 `
 
-const StyledTitle = styled(Header2)`
-  text-align: center;
-  color: #fff;
-  font-size: 3rem;
-  position: relative;
-  z-index: 3;
-
+const StyledTitle = styled.div`
+  background: url('assets/background/sponsors/sponsors-header.svg') no-repeat right;
+  background-size: 38vw;
+  width: 38vw;
+  height: 10vw;
+  /* font-size: 3rem; */
+  position: absolute;
+  z-index: 10;
+  left: 50vw;
+  top: 1vw;
+  transform: translateX(-50%);
   ${p => p.theme.mediaQueries.mobile} {
     font-size: 2em;
   }
@@ -67,32 +70,16 @@ const colorChange = keyframes`
   }
 `
 
-const UFO = styled.div`
-  /* background: url('assets/background/sponsors/ufo.png') no-repeat center; */
-  width: 100vw;
-  height: 100vw;
-  aspect-ratio: 1293/564.05;
-  transform: translateY(-15vw);
+const TitleSponsorContainer = styled.div`
+  background: url('assets/background/sponsors/title-sponsor-container.svg') no-repeat right;
+  background-size: 48vw;
+  width: 48vw;
+  height: 20vw;
   position: absolute;
-  z-index: 2;
-
-  ${p => p.theme.mediaQueries.mobile} {
-    display: none;
-  }
-`
-
-const UFOLight = styled.div`
-  /* background: url('assets/background/sponsors/light.png') no-repeat center; */
-  width: 100vw;
-  height: 100vw;
-  aspect-ratio: 1/1.5;
-  top: 15%;
-  position: absolute;
-  z-index: -1;
-  animation: ${colorChange} 5s infinite; // apply the animation
-  ${p => p.theme.mediaQueries.mobile} {
-    display: none;
-  }
+  z-index: 10;
+  left: 25vw;
+  top: 12vw;
+  transform: translateX(-50%);
 `
 
 const Sponsors = styled.div`
@@ -101,8 +88,8 @@ const Sponsors = styled.div`
   justify-content: center;
   align-items: center;
   width: 50vw;
-  height: 30vw;
-  padding-top: 70vw;
+  height: 20vw;
+  padding-top: 30vw;
   position: relative;
   z-index: 3;
   margin: auto;
@@ -110,11 +97,24 @@ const Sponsors = styled.div`
 
 const SponsorTier = styled.div`
   display: flex;
+  position: absolute;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  max-height: 15vw;
-  gap: 2.5vw;
+  background-color: #130f2a;
+  padding: 8px 20px;
+  height: 13vw;
+  background-size: 100%;
+  gap: 1.3vw;
+`
+
+const TitleSponsorLink = styled.a`
+  display: flex;
+  align-items: center;
+  flex: 1 1 0px;
+  position: absolute;
+  top: 7vw;
+  right: 33vw;
 `
 
 const SponsorLink = styled.a`
@@ -123,19 +123,41 @@ const SponsorLink = styled.a`
   flex: 1 1 0px;
 `
 
+const SponsorText = styled.h1`
+  font-size: 1vw;
+  width: 40vw;
+  color: black;
+  position: absolute;
+  top: 10.7vw;
+  font-weight: lighter;
+  right: 3.9vw;
+  font-family: 'HK Grotesk';
+`
+
+const TitleSponsorLogo = styled.img`
+  position: absolute;
+  width: 17vw;
+`
+
 const SponsorLogo = styled.img`
-  max-width: 100%;
+  /* max-width: 100%; */
+  background-color: white;
+  border-radius: 8px;
+  height: 6vw;
+  width: 15vw;
+  padding: 1vw;
+  object-fit: contain;
 `
 
 const PushinP = styled.p`
   color: #fff;
   text-align: left;
-  width: 30vw;
+  width: 39vw;
   min-width: 500px;
   margin: 0 auto;
-  padding-top: 2rem;
-  max-width: 800px;
+  padding-top: 1vw;
   position: relative;
+  top: 10vw;
   z-index: 3;
 
   ${p => p.theme.mediaQueries.mobile} {
@@ -160,6 +182,17 @@ const Skip = styled.div`
   }
 `
 
+const EmailBlurb = styled.a`
+  color: #f5c745;
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
 export default function Sponsor() {
   const [sponsors, setSponsors] = useState({})
 
@@ -179,40 +212,56 @@ export default function Sponsor() {
 
   return Object.keys(sponsors).length > 0 ? (
     <BgSectionContainer id="sponsors">
-      <UFO></UFO>
-      <UFOLight />
+      <StyledTitle />
+      <PushinP>
+        nwPlus is always looking for new ventures, opportunities, and connections. If you are interested in working with
+        us, joining us or speaking at one of our events, feel free to reach out to use at{' '}
+        <EmailBlurb href="mailto:sponsorship@nwplus.io" target="_blank" rel="noopener noreferrer">
+          sponsorship@nwplus.io
+        </EmailBlurb>
+      </PushinP>
       <Sponsors>
-        <StyledTitle>Sponsors</StyledTitle>
         {Object.keys(sponsors)
           .sort((a, b) => sponsorTierOrder[a] - sponsorTierOrder[b])
-          .map(key => (
-            <SponsorTier>
-              {sponsors[key].map(sponsor => (
-                <SponsorLink href={sponsor.link} target="_blank" rel="noreferrer">
-                  <SponsorLogo src={sponsor.imgURL} />
-                </SponsorLink>
-              ))}
-            </SponsorTier>
-          ))}
-        <PushinP>
-          Sponsors make this event happen. If you are interested in working with us, joining us or speaking at one of
-          our events, please reach out to us!
-        </PushinP>
-        <ButtonContainer>
-          <Button
-            target="_blank"
-            rel="noopener noreferrer"
-            href="mailto:sponsorship@nwplus.io?Subject=Sponsorship"
-            width="205px"
-            height="50px"
-            borderRadius="6px"
-            textColor="#FFFFFF"
-            backgroundColor="linear-gradient(to left, #959AFB, #9AD4DE)"
-            isHover
-          >
-            Sponsor HackCamp!
-          </Button>
-        </ButtonContainer>
+          .map((key, index) => {
+            const row = index
+            if (key === 'platinum') {
+              return (
+                <TitleSponsorContainer key={key}>
+                  {sponsors[key].map(sponsor => (
+                    <>
+                      <TitleSponsorLink href={sponsor.link} target="_blank" rel="noreferrer">
+                        <TitleSponsorLogo src={sponsor.imgURL} />
+                      </TitleSponsorLink>
+                      <SponsorText>{sponsor.blurb}</SponsorText>
+                    </>
+                  ))}
+                </TitleSponsorContainer>
+              )
+            } else {
+              const sponsorCount = sponsors[key].length
+
+              const ContainerSVG = `assets/background/sponsors/${sponsorCount}-sponsors.svg`
+
+              return (
+                <SponsorTier
+                  key={key}
+                  style={{
+                    backgroundImage: `url(${ContainerSVG})`,
+                    backgroundRepeat: 'no-repeat',
+                    top: `${19 + row * 14}vw`,
+                    backgroundPosition: 'center',
+                  }}
+                >
+                  {sponsors[key].map(sponsor => (
+                    <SponsorLink href={sponsor.link} target="_blank" rel="noreferrer">
+                      <SponsorLogo src={sponsor.imgURL} />
+                    </SponsorLink>
+                  ))}
+                </SponsorTier>
+              )
+            }
+          })}
       </Sponsors>
     </BgSectionContainer>
   ) : (
