2f6a2d183fff1b74a652fd419ea07162243fc1b4
martincai8 <martincai46@gmail.com>
2024-11-16 16:04:04 -0800

    mobile about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,20 @@
 import styled from 'styled-components'
 // import { TABLET } from '@constants/measurements'
 
+const AboutContainer = styled.div`
+  aspect-ratio: 1280/849;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    aspect-ratio: 487 / 922;
+  }
+`
+
 const AboutBackground = styled.div`
   background-image: url('./assets/images/about_background.png');
   background-size: contain;
@@ -14,22 +28,9 @@ const AboutBackground = styled.div`
   width: 100%;
   height: 100%;
   z-index: -1;
-`
-
-const AboutContainer = styled.div`
-  aspect-ratio: 1280/849;
-  height: 100%;
-  position: relative;
-  z-index: 1;
-  display: flex;
-  align-items: center;
-  width: 100%;
 
   ${p => p.theme.mediaQueries.mobile} {
-    // margin-top: 10rem;
-    // aspect-ratio: 428 / 700;
-    // align-items: center;
-    // margin-bottom: 16rem;
+    background-image: url('./assets/images/mobile_about_background.png');
   }
 `
 
@@ -46,12 +47,24 @@ const TextContainer = styled.div`
   z-index: 11;
   transform: perspective(1000px) rotateX(2deg);
   transform-origin: center top;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    left: 50%;
+    transform: translateX(-50%);
+    top: calc(100vw * (485 / 487));
+    width: calc(100vw * (335 / 487));
+    gap: calc(100vw * (20 / 487));
+  }
 `
 
 const Title = styled.p`
   font-family: 'LT Museum';
   font-size: calc(100vw * (30 / 1280));
   font-weight: 700;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    font-size: calc(100vw * (30 / 487));
+  }
 `
 
 const Description = styled.p`
@@ -59,22 +72,26 @@ const Description = styled.p`
   font-size: calc(100vw * (16 / 1280));
   font-weight: 400;
   line-height: 1.5;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    font-size: calc(100vw * (18 / 487));
+  }
 `
 
 const About = () => (
-    <AboutContainer id="about">
-      <AboutBackground />
-      <TextContainer>
-        <Title>One for the history books</Title>
-        <Description>
-          Celebrate the 10th anniversary of nwHacks with us! Everyone is welcome at nwHacks, whether you are just
-          getting into tech or are a seasoned hacker. Join us in-person on January 18-19, 2025 for a weekend of
-          creativity, community, and innovation. All you need is an open mind and an insatiable desire to learn - we&apos;ll
-          take care of the rest. Create a project, learn new skills, and bond with friends, all in 24 hours! This one
-          will go down in history.
-        </Description>
-      </TextContainer>
-    </AboutContainer>
-  )
+  <AboutContainer id="about">
+    <AboutBackground />
+    <TextContainer>
+      <Title>One for the history books</Title>
+      <Description>
+        Celebrate the 10th anniversary of nwHacks with us! Everyone is welcome at nwHacks, whether you are just getting
+        into tech or are a seasoned hacker. Join us in-person on January 18-19, 2025 for a weekend of creativity,
+        community, and innovation. All you need is an open mind and an insatiable desire to learn - we&apos;ll take care
+        of the rest. Create a project, learn new skills, and bond with friends, all in 24 hours! This one will go down
+        in history.
+      </Description>
+    </TextContainer>
+  </AboutContainer>
+)
 
 export default About
