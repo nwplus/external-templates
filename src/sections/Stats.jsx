5fa35061b619ce9843422b0313f3b39c0d6caa0b
martincai8 <martincai46@gmail.com>
2024-11-17 15:03:22 -0800

    stats animation

--- src/sections/Stats.jsx
+++ src/sections/Stats.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
-import { useParallax } from 'react-scroll-parallax'
-
-import lightStatsImage from '@assets/images/LightStats.svg'
-import unlightStatsImage from '@assets/images/UnlightStats.svg'
 import tabletStatsImage from '@assets/images/tabletStats.png'
 import mobileStatsImage from '@assets/images/mobileStats.png'
 import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
+import StatsBoxes from '@components/StatsBoxes'
+
+const OuterContainer = styled.div`
+  position: relative;
+`
 
 const StatsContainer = styled.div`
   width: 100vw;
   aspect-ratio: 1280/1280;
   height: auto;
   position: relative;
+  z-index: 2;
 
   ${p => p.theme.mediaQueries.tablet} {
     display: none;
@@ -21,21 +23,14 @@ const StatsContainer = styled.div`
 
 const MobileTabletStatsContainer = styled.div`
   display: none;
-  width: 100%;
+  width: 100vw;
   height: auto;
   position: relative;
 
   ${p => p.theme.mediaQueries.tablet} {
     display: block;
+    aspect-ratio: 1280/1280;
   }
-`
-
-const StatsImg = styled.img`
-  position: absolute;
-  width: 100%;
-  height: auto;
-  opacity: ${p => (p.isHidden ? 1 : 0)};
-  transition: opacity 0.5s ease;
 `
 
 const MobileTabletImg = styled.img`
@@ -43,22 +38,35 @@ const MobileTabletImg = styled.img`
   height: auto;
 `
 
-const HiddenTitle = styled.p`
-  font-family: 'LT Museum';
-  color: white;
-  font-size: calc(100vw * (50 / 1280));
-  font-weight: 700;
+const Title = styled.p`
+  color: ${p => (p.isGlowing ? 'white' : '#B4B4B4')};
+  text-shadow: ${p => (p.isGlowing ? '0 0 32px rgba(255, 255, 255, 0.5)' : 'none')};
+  font-weight: 900;
+
   position: absolute;
-  top: calc(100vw * (120 / 1280));
-  left: 10%;
-  z-index: 1;
-  opacity: 0;
+  top: calc(100vw * (100 / 1280));
+  font-size: calc(100vw * (56 / 1280));
+  left: calc(100vw * (100 / 1280));
+
+  ${p => p.theme.mediaQueries.tablet} {
+    width: 100%;
+    font-size: calc(100vw * (56 / 834));
+    font-weight: 700;
+    top: calc(100vw * (40 / 834));
+    left: 0;
+    z-index: 1;
+    text-align: center;
+  }
+
+  ${p => p.theme.mediaQueries.mobile} {
+    font-size: calc(100vw * (42 / 487));
+  }
 `
 
-// Stats component
 const Stats = () => {
   const [isMobile, setIsMobile] = useState(false)
   const [isTablet, setIsTablet] = useState(false)
+  const [titleGlow, setTitleGlow] = useState(false)
   const statsContainerRef = useRef(null)
 
   useEffect(() => {
@@ -75,25 +83,43 @@ const Stats = () => {
     }
   }, [])
 
-  const unlight = useParallax({})
-  const light = useParallax({})
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          const lightElements = document.getElementsByClassName('light')
+          Array.from(lightElements).forEach(element => {
+            element.style.opacity = entry.isIntersecting ? '1' : '0'
+          })
+          setTitleGlow(entry.isIntersecting)
+        })
+      },
+      { threshold: 0.4 }
+    )
+
+    if (statsContainerRef.current) {
+      observer.observe(statsContainerRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [])
 
   return (
-    <StatsContainer id="stats">
+    <OuterContainer id="stats">
       {!isMobile && !isTablet && (
         <StatsContainer ref={statsContainerRef}>
-          <HiddenTitle>Last year we had...</HiddenTitle>
-          <StatsImg src={unlightStatsImage} ref={unlight.ref} isHidden={false} />
-          <StatsImg src={lightStatsImage} ref={light.ref} isHidden />
+          <StatsBoxes />
+          <Title isGlowing={titleGlow}>Last year we had...</Title>
         </StatsContainer>
       )}
 
       {(isMobile || isTablet) && (
         <MobileTabletStatsContainer>
+          <Title isGlowing={true}>Last year we had...</Title>
           <MobileTabletImg src={isMobile ? mobileStatsImage : tabletStatsImage} alt="Mobile or Tablet Stats" />
         </MobileTabletStatsContainer>
       )}
-    </StatsContainer>
+    </OuterContainer>
   )
 }
 
