202e8f21621d816d4c8b78bd38ef1ead55fcb457
martincai8 <martincai46@gmail.com>
2024-11-16 22:41:41 -0800

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,12 +10,18 @@ import {
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons'
 import Team from '@components/Team'
+import Newsletter from '@components/Newsletter'
 import confetti from '../../public/assets/images/confetti.svg'
 
 const FooterContainer = styled.div`
   position: relative;
   aspect-ratio: 1280/1150;
   width: 100%;
+  overflow: hidden;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    aspect-ratio: 487/820;
+  }
 `
 
 const FooterBackground = styled.div`
@@ -30,52 +36,55 @@ const FooterBackground = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    background-image: url('./assets/images/footer_background_mobile.png');
+  }
 `
 
 const SocialMediaIcons = styled.div`
   justify-content: center;
   align-items: center;
   display: flex;
+  gap: calc(100vw * (40 / 1280));
 
   a {
-    width: 50px;
+    width: calc(100vw * (53 / 1280));
     text-decoration: none;
-    color: #eac669;
+    color: #f0d4a1;
 
     &:hover {
-      color: #aed1e1;
+      color: #eac669;
     }
   }
 
-  gap: 2rem;
-
   ${p => p.theme.mediaQueries.mobile} {
+    width: 100%;
     a {
-      width: 25px;
+      width: calc(100vw * (40 / 487));
     }
-    gap: 1rem;
+    justify-content: space-evenly;
   }
 `
 
 const Links = styled.div`
   display: flex;
-  gap: 2rem;
-  font-size: 1.5rem;
+  gap: calc(100vw * (20 / 1280));
+  font-size: calc(100vw * (27 / 1280));
 
   a {
     font-weight: 600;
-    color: #eac669;
+    color: #f0d4a1;
 
     &:hover {
-      color: #aed1e1;
+      color: #eac669;
     }
   }
 
   ${p => p.theme.mediaQueries.mobile} {
-    font-size: 1.1rem;
-    flex-wrap: wrap;
-    justify-content: center;
-    row-gap: 1rem;
+    width: 100%;
+    font-size: calc(100vw * (20 / 487));
+    justify-content: space-evenly;
 
     a {
       white-space: nowrap;
@@ -89,29 +98,25 @@ const TextContainer = styled.div`
   justify-content: center;
   width: 100%;
   align-items: center;
-  padding-top: 0%;
-  gap: 1rem;
+  padding-top: calc(100vw * (40 / 1280));
+  gap: calc(100vw * (16 / 1280));
 
   ${p => p.theme.mediaQueries.mobile} {
-    padding-top: 65%;
+    padding-top: calc(100vw * (90 / 487));
   }
 `
 
 const SmallText = styled.div`
   user-select: none;
-  padding-top: 2.5rem;
+  padding-top: calc(100vw * (15 / 1280));
   text-align: center;
 
-  div {
-    color: #aed1e1;
-    font-size: 1.2rem;
-    font-weight: 500;
-  }
+  color: #f9c745;
+  font-size: calc(100vw * (14 / 1280));
+  font-weight: 600;
 
   ${p => p.theme.mediaQueries.mobile} {
-    div {
-      font-size: 1rem;
-    }
+    font-size: calc(100vw * (12 / 487));
   }
 `
 
@@ -151,67 +156,66 @@ const confettiFall = keyframes`
     transform: translate(-50%, -100%);
   }
   100% {
-    transform: translate(-50%, 100%);
+    transform: translate(-50%, 130%);
   }
-`;
+`
 
 const ConfettiImage = styled.img`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 50%;
   transform: translate(-50%, -100%);
   width: 100%;
   height: auto;
-  animation: ${confettiFall} 5s forwards ease-in-out;
+  animation: ${confettiFall} 4s forwards ease-in-out;
   z-index: 9999;
   pointer-events: none;
-`;
+
+  ${p => p.theme.mediaQueries.mobile} {
+    display: none;
+  }
+`
 
 const Footer = () => {
-  const [showConfetti, setShowConfetti] = useState(false);
-  const footerRef = useRef(null);
+  const [showConfetti, setShowConfetti] = useState(false)
+  const footerRef = useRef(null)
 
   useEffect(() => {
     const observerOptions = {
       root: null,
       rootMargin: '0px',
       threshold: 0.1,
-    };
+    }
 
-    const observerCallback = (entries) => {
-      const [entry] = entries;
+    const observerCallback = entries => {
+      const [entry] = entries
       if (entry.isIntersecting) {
-        setShowConfetti(true);
+        setShowConfetti(true)
       }
-    };
+    }
 
-    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    const observer = new IntersectionObserver(observerCallback, observerOptions)
 
     if (footerRef.current) {
-      observer.observe(footerRef.current);
+      observer.observe(footerRef.current)
     }
 
     return () => {
       if (footerRef.current) {
-        observer.unobserve(footerRef.current);
+        observer.unobserve(footerRef.current)
       }
-    };
-  }, []);
+    }
+  }, [])
 
   const handleAnimationEnd = () => {
-    setShowConfetti(false);
-  };
+    setShowConfetti(false)
+  }
 
   return (
     <FooterContainer ref={footerRef}>
       <FooterBackground />
-      {showConfetti && (
-        <ConfettiImage
-          src={confetti}
-          alt="Confetti"
-          onAnimationEnd={handleAnimationEnd}
-        />
-      )}
+      {showConfetti && <ConfettiImage src={confetti} alt="Confetti" onAnimationEnd={handleAnimationEnd} />}
+
       <StaticContainer>
         <TextContainer>
           <SocialMediaIcons>
@@ -245,17 +249,15 @@ const Footer = () => {
               Code of Conduct
             </a>
           </Links>
-          <SmallText>
-            <div>Organized and held by nwPlus</div>
-            <div>Copyright &copy; nwHacks 2025</div>
-          </SmallText>
+          <Newsletter />
         </TextContainer>
         <TeamContainer>
           <Team />
+          <SmallText>Copyright &copy; nwHacks 2025</SmallText>
         </TeamContainer>
       </StaticContainer>
     </FooterContainer>
   )
-};
+}
 
 export default Footer
