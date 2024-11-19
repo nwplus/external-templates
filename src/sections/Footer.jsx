bb709ee73a9e186e37875e6ab24f16076f7dea3a
martincai8 <martincai46@gmail.com>
2024-11-18 17:23:53 -0800

    smaller confetti

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -156,8 +156,8 @@ const ConfettiContainer = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  width: 200vw;
-  height: 200vh;
+  width: 100vw;
+  height: 80vh;
 
   canvas {
     width: 100% !important;
