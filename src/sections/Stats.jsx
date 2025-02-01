ff93c38fdc149c3ac8eaf573e82eef51e23dcbc0
kashgarg <kashishgarg247@gmail.com>
2025-02-01 14:40:12 -0800

    Stats section - desktop

--- src/sections/Stats.jsx
+++ src/sections/Stats.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import tabletStatsImage from '@assets/images/tabletStats.png'
 import mobileStatsImage from '@assets/images/mobileStats.png'
 import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
-import StatsBoxes from '@components/StatsBoxes'
 
 const OuterContainer = styled.div`
   position: relative;
@@ -11,7 +10,7 @@ const OuterContainer = styled.div`
 
 const StatsContainer = styled.div`
   width: 100vw;
-  aspect-ratio: 1280/1280;
+  aspect-ratio: 1920/1248;
   height: auto;
   position: relative;
   z-index: 2;
@@ -29,7 +28,7 @@ const MobileTabletStatsContainer = styled.div`
 
   ${p => p.theme.mediaQueries.tablet} {
     display: block;
-    aspect-ratio: 1280/1280;
+    aspect-ratio: 1920/1280;
   }
 `
 
@@ -39,14 +38,17 @@ const MobileTabletImg = styled.img`
 `
 
 const Title = styled.p`
-  color: ${p => (p.isGlowing ? 'white' : '#B4B4B4')};
-  text-shadow: ${p => (p.isGlowing ? '0 0 32px rgba(255, 255, 255, 0.5)' : 'none')};
-  font-weight: 900;
+  color: #A6321E;
+  font-family: Gloock;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 
   position: absolute;
-  top: calc(100vw * (100 / 1280));
-  font-size: calc(100vw * (56 / 1280));
-  left: calc(100vw * (100 / 1280));
+  top: calc(100vw * (100 / 1920));
+  font-size: calc(100vw * (56 / 1920));
+  left: calc(100vw * (415 / 1920));
 
   ${p => p.theme.mediaQueries.tablet} {
     width: 100%;
@@ -63,10 +65,33 @@ const Title = styled.p`
   }
 `
 
+const Bowl = styled.img`
+  position: absolute;
+  height: auto;
+  width: calc(100vw * (1275 / 1920));
+  top: calc(100vw * (100 / 1920));
+  left: calc(100vw * (0 / 1920));
+
+  ${p => p.theme.mediaQueries.mobile} {
+    width: calc(100vw * (100 / 393));
+    top: calc(100vw * (50 / 393));
+    left: calc(100vw * (150 / 393));
+  }
+`
+
+const StatImage = styled.img`
+  position: absolute;
+  height: auto;
+  width: ${({ width }) => `calc(100vw * (${width} / 1920))`};
+  top: ${({ top }) => `calc(100vw * (${top} / 1920))`};
+  left: ${({ left, scrollOffset }) => `calc(100vw * (${left} / 1920) + ${scrollOffset}px)`};
+  transition: left 0.1s linear;
+`;
+
 const Stats = () => {
   const [isMobile, setIsMobile] = useState(false)
   const [isTablet, setIsTablet] = useState(false)
-  const [titleGlow, setTitleGlow] = useState(false)
+  const [scrollOffset, setScrollOffset] = useState(0);
   const statsContainerRef = useRef(null)
 
   useEffect(() => {
@@ -92,7 +117,6 @@ const Stats = () => {
             const newOpacity = entry.isIntersecting ? '1' : '0'
             element.setAttribute('style', `opacity: ${newOpacity}`)
           })
-          setTitleGlow(entry.isIntersecting)
         })
       },
       { threshold: 0.4 }
@@ -105,18 +129,32 @@ const Stats = () => {
     return () => observer.disconnect()
   }, [])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const {scrollY} = window;
+      setScrollOffset(scrollY * 0.10);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <OuterContainer id="stats">
       {!isMobile && !isTablet && (
         <StatsContainer ref={statsContainerRef}>
-          <StatsBoxes />
-          <Title isGlowing={titleGlow}>Last year we had...</Title>
+          <Title>Last year we had...</Title>
+          <Bowl src='assets/images/about/bowl.svg'/>
+          <StatImage src="assets/images/about/egg_stat.svg" width={370} top={300} left={190} scrollOffset={scrollOffset} />
+          <StatImage src="assets/images/about/bottle_stat.svg" width={310} top={230} left={660} scrollOffset={scrollOffset} />
+          <StatImage src="assets/images/about/flour_bag.svg" width={400} top={400} left={1000} scrollOffset={scrollOffset} />
+          <StatImage src="assets/images/about/butter_stat.svg" width={520} top={750} left={840} scrollOffset={scrollOffset} />
         </StatsContainer>
       )}
 
       {(isMobile || isTablet) && (
         <MobileTabletStatsContainer>
-          <Title isGlowing>Last year we had...</Title>
+          <Title>Last year we had...</Title>
           <MobileTabletImg src={isMobile ? mobileStatsImage : tabletStatsImage} alt="Mobile or Tablet Stats" />
         </MobileTabletStatsContainer>
       )}
