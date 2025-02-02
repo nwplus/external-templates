0c1dd082daa58b2c133ce056a000024d56a86913
kashgarg <kashishgarg247@gmail.com>
2025-02-02 13:40:14 -0800

    Photo Gallery - drag animation

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Draggable from 'react-draggable';
 
 // Styled Components
 const GalleryContainer = styled.div`
@@ -70,45 +71,57 @@ const RecapContainer = styled.div`
   }
 `
 
-const VideoPolaroid = styled.img`
+const VideoWrapper = styled.div`
   position: absolute;
-  height: auto;
+  width: 100%;
+  height: calc(100vw * (400 / 1920));
+  top: 0;
+  left: 0;
+`
+
+const VideoComponent = styled.div`
+  position: relative;
   width: calc(100vw * (830 / 1920));
-  top: calc(100vw * (100 / 1920));
-  left: calc(100vw * (145 / 1920)); 
-`
-
-const VideoContainer = styled.div`
-  width: calc(100vw * (760 / 1920));
-  height: calc(100vw * (446 / 1920));
-  padding: calc(100vw * (12 / 1920));
-  transform: rotate(-2.303deg);
-  display: block;
-  margin: 0 auto;
-  background: #151515;
-  position: relative;
-  top: calc(100vw * (140 / 1920));
-  left: calc(100vw * (-403 / 1920));
-  overflow: visible;
+  height: auto;
+  cursor: grab;
   z-index: 2;
+  position: absolute;
+  top: calc(100vw * (100 / 1920));
+  left: calc(100vw * (180 / 1920));
 
-  ${p => p.theme.mediaQueries.tablet} {
-    top: calc(100vw * (350 / 893));
-    width: calc(100vw * (446 / 893));
-    height: calc(100vw * (296 / 893));
-    border: calc(100vw * (10 / 893)) solid #151515;
+  .polaroid {
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
   }
 
-  ${p => p.theme.mediaQueries.mobile} {
-    top: calc(100vw * (500 / 487));
-    width: calc(100vw * (446 / 487));
-    height: calc(100vw * (296 / 487));
-    border: calc(100vw * (10 / 487)) solid #151515;
+  .video-container {
+    width: calc(100vw * (760 / 1920));
+    height: calc(100vw * (446 / 1920));
+    padding: calc(100vw * (12 / 1920));
+    transform: rotate(-2.303deg);
+    background: #151515;
+    position: absolute;
+    top: calc(100vw * (40 / 1920));
+    left: calc(100vw * (35 / 1920));
+    overflow: visible;
+
+    ${p => p.theme.mediaQueries.tablet} {
+      top: calc(100vw * (350 / 893));
+      width: calc(100vw * (446 / 893));
+      height: calc(100vw * (296 / 893));
+      border: calc(100vw * (10 / 893)) solid #151515;
+    }
+
+    ${p => p.theme.mediaQueries.mobile} {
+      top: calc(100vw * (500 / 487));
+      width: calc(100vw * (446 / 487));
+      height: calc(100vw * (296 / 487));
+      border: calc(100vw * (10 / 487)) solid #151515;
+    }
   }
-`
-const VideoSection = styled.div`
-  position: relative;
-  z-index: 3;
 `
 
 const TooltipText = styled.div`
@@ -141,6 +154,7 @@ const BreadOne = styled.img`
   top: calc(100vw * (60 / 1920));
   left: calc(100vw * (505 / 1920)); 
   z-index: 4;
+  cursor: grab;
 `
 
 const BreadTwo = styled.img`
@@ -150,6 +164,7 @@ const BreadTwo = styled.img`
   top: calc(100vw * (600 / 1920));
   left: calc(100vw * (925 / 1920)); 
   z-index: 13;
+  cursor: grab;
 `
 
 const BreadThree = styled.img`
@@ -159,6 +174,7 @@ const BreadThree = styled.img`
   top: calc(100vw * (200 / 1920));
   left: calc(100vw * (1130 / 1920)); 
   z-index: 15;
+  cursor: grab;
 `
 
 const Croissant = styled.img`
@@ -167,7 +183,8 @@ const Croissant = styled.img`
   width: calc(100vw * (280 / 1920));
   top: calc(100vw * (470 / 1920));
   left: calc(100vw * (1330 / 1920));
-  z-index: 12; 
+  z-index: 12;
+  cursor: grab;
 `
 
 const PolaroidSelfie = styled.img`
@@ -177,6 +194,7 @@ const PolaroidSelfie = styled.img`
   top: calc(100vw * (450 / 1920));
   left: calc(100vw * (80 / 1920)); 
   z-index: 5;
+  cursor: grab;
 `
 
 const PolaroidCeremony = styled.img`
@@ -186,6 +204,7 @@ const PolaroidCeremony = styled.img`
   top: calc(100vw * (580 / 1920));
   left: calc(100vw * (720 / 1920)); 
   z-index: 6;
+  cursor: grab;
 `
 
 const PolaroidPanel = styled.img`
@@ -195,6 +214,7 @@ const PolaroidPanel = styled.img`
   top: calc(100vw * (200 / 1920));
   left: calc(100vw * (930 / 1920)); 
   z-index: 14;
+  cursor: grab;
 `
 
 const PolaroidHeart = styled.img`
@@ -204,6 +224,7 @@ const PolaroidHeart = styled.img`
   top: calc(100vw * (500 / 1920));
   left: calc(100vw * (1040 / 1920)); 
   z-index: 7;
+  cursor: grab;
 `
 
 const PolaroidWinners = styled.img`
@@ -213,6 +234,7 @@ const PolaroidWinners = styled.img`
   top: calc(100vw * (50 / 1920));
   left: calc(100vw * (1240 / 1920)); 
   z-index: 8;
+  cursor: grab;
 `
 
 const PolaroidMerch = styled.img`
@@ -222,6 +244,7 @@ const PolaroidMerch = styled.img`
   top: calc(100vw * (320 / 1920));
   left: calc(100vw * (1450 / 1920)); 
   z-index: 9;
+  cursor: grab;
 `
 
 const PolaroidAtrium = styled.img`
@@ -231,6 +254,7 @@ const PolaroidAtrium = styled.img`
   top: calc(100vw * (600 / 1920));
   left: calc(100vw * (1280 / 1920)); 
   z-index: 10;
+  cursor: grab;
 `
 
 const Nugget = styled.img`
@@ -239,7 +263,8 @@ const Nugget = styled.img`
   width: calc(100vw * (300 / 1920));
   top: calc(100vw * (720 / 1920));
   left: calc(100vw * (1600 / 1920));
-  z-index: 11; 
+  z-index: 11;
+  cursor: grab;
 `
 
 // Component
@@ -247,42 +272,69 @@ const Gallery = () => (
   <GalleryContainer>
     <InfiniteScrollWrapper>
       <InfiniteScrollText>
-        2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded
+        2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded
         2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded
         2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded
         2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded 2024 Unfolded
       </InfiniteScrollText>
     </InfiniteScrollWrapper>
     <RecapContainer>
-      <BreadOne src="assets/images/gallery/bread_one.svg" />
-      <BreadTwo src="assets/images/gallery/bread_two.svg" />
-      <BreadThree src="assets/images/gallery/bread_three.svg" />
-      <Croissant src="assets/images/gallery/croissant.svg" />
-      <PolaroidSelfie src="assets/images/gallery/polaroid_selfie.png" />
-      <PolaroidCeremony src="assets/images/gallery/polaroid_ceremony.png" />
-      <PolaroidPanel src="assets/images/gallery/polaroid_panel.png" />
-      <PolaroidHeart src="assets/images/gallery/polaroid_heart.png" />
-      <PolaroidWinners src="assets/images/gallery/polaroid_winners.png" />
-      <PolaroidMerch src="assets/images/gallery/polaroid_merch.png" />
-      <PolaroidAtrium src="assets/images/gallery/polaroid_atrium.png" />
-      <Nugget src="assets/images/gallery/nugget.svg" />
-      <VideoSection>
-        <VideoPolaroid src="assets/images/gallery/video_polaroid.svg" />
-        <VideoContainer>
-        <iframe
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/-N_ds4fcVL4?si=-4UJKRL8_e2l3u-P"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          style={{ position: 'absolute', top: 0, left: 0 }} />
-        </VideoContainer>
-      </VideoSection>
-      <TooltipImg src="assets/images/gallery/tooltip.svg" />
-      <TooltipText>Drag the polaroids and magnets as you like.</TooltipText>
+        <Draggable bounds="parent">
+          <BreadOne src="assets/images/gallery/bread_one.svg" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <BreadTwo src="assets/images/gallery/bread_two.svg" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <BreadThree src="assets/images/gallery/bread_three.svg" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <Croissant src="assets/images/gallery/croissant.svg" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidSelfie src="assets/images/gallery/polaroid_selfie.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidCeremony src="assets/images/gallery/polaroid_ceremony.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidPanel src="assets/images/gallery/polaroid_panel.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidHeart src="assets/images/gallery/polaroid_heart.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidWinners src="assets/images/gallery/polaroid_winners.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidMerch src="assets/images/gallery/polaroid_merch.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <PolaroidAtrium src="assets/images/gallery/polaroid_atrium.png" />
+        </Draggable>
+        <Draggable bounds="parent">
+          <Nugget src="assets/images/gallery/nugget.svg" />
+        </Draggable>
+        <VideoWrapper>
+          <Draggable bounds="parent">
+            <VideoComponent>
+              <img className="polaroid" src="assets/images/gallery/video_polaroid.svg" alt="video polaroid" />
+              <div className="video-container">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/-N_ds4fcVL4?si=-4UJKRL8_e2l3u-P"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                  style={{ position: 'absolute', top: 0, left: 0 }} />
+              </div>
+            </VideoComponent>
+          </Draggable>
+        </VideoWrapper>
+        <TooltipImg src="assets/images/gallery/tooltip.svg" />
+        <TooltipText>Drag the polaroids and magnets as you like.</TooltipText>
     </RecapContainer>
   </GalleryContainer>
 )
