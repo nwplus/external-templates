9293a4a05c68af323bd80b845a9edba501562c0b
tdanielles <trisha.d.sia@gmail.com>
2025-02-01 22:16:37 -0800

    Plates and stands

--- src/components/SponsorsGrid.jsx
+++ src/components/SponsorsGrid.jsx
@@ -1,7 +1,18 @@
 import { useEffect, useState, useRef, memo } from 'react'
 import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
 import styled from 'styled-components'
-import Floor from './Floor'
+import startingstand from '@assets/images/sponsors/startingstand.svg'
+import normalstand from '@assets/images/sponsors/normalstand.svg'
+import platplate from '@assets/images/sponsors/platplate.svg'
+import goldplate from '@assets/images/sponsors/goldplate.svg'
+import silverplate from '@assets/images/sponsors/silverplate.svg'
+import bronzeplate from '@assets/images/sponsors/bronzeplate.svg'
+import startupplate from '@assets/images/sponsors/startupplate.svg'
+import platpastry from '@assets/images/sponsors/platpastry.svg'
+import goldpastry from '@assets/images/sponsors/goldpastry.svg'
+import silverpastry from '@assets/images/sponsors/silverpastry.svg'
+import bronzepastry from '@assets/images/sponsors/bronzepastry.svg'
+import startuppastry from '@assets/images/sponsors/startuppastry.svg'
 
 const SPONSOR_WIDTH = { title: 70, platinum: 45, gold: 40, silver: 35, bronze: 30, startup: 25, inkind: 20 }
 const MOBILE_SPONSOR_WIDTH = { title: 95, platinum: 80, gold: 45, silver: 35, bronze: 30, startup: 25, inkind: 20 }
@@ -42,17 +53,20 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: calc(100vw * (120 / 1280));
+  gap: calc(100vw * (80 / 1920));
+  margin-top: calc(100vw * (150 / 1920));
 `
 
 const SponsorLevelContainer = styled.div`
   position: relative;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
+  // margin: 0 auto;
+  // display: flex;
 `
 
 const Row = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -69,14 +83,21 @@ const Row = styled.div`
 
 const SponsorContainer = styled.div`
   width: ${p => p.size}%;
-  aspect-ratio: 769 / 384;
-  background-image: url(./assets/images/sponsor_card.svg);
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  z-index: 2;
+  // aspect-ratio: 769 / 384;
+  // background-size: contain;
+  // background-repeat: no-repeat;
+  // background-position: center;
+  // z-index: 2;
+  // display: flex;
+  // justify-content: center;
+
+  position: relative;
+  width: auto;
+  max-width: 90%;
   display: flex;
+  align-items: center;
   justify-content: center;
+  margin-top: 5%;
 
   ${p => p.theme.mediaQueries.mobile} {
     width: ${p => MOBILE_SPONSOR_WIDTH[p.tier]}%;
@@ -94,44 +115,131 @@ const SponsorLink = styled.a`
 `
 
 const SponsorImg = styled.img`
+  position: absolute;
   height: 100%;
   max-width: 70%;
   border: none;
   object-fit: contain;
+  z-index: 2;
+  top: 10%;
 `
 
-const Nugget = styled.img`
-  position: absolute;
-  width: calc(100vw * (135 / 1280));
+const PastryImage = styled.img`
+  width: ${props => props.length};
   height: auto;
-  z-index: 3;
-  left: calc(100vw * (475 / 1280));
-  top: calc(100vw * (275 / 1280));
-
-  ${p => p.theme.mediaQueries.mobile} {
-    width: calc(100vw * (70 / 487));
-    left: calc(100vw * (150 / 487));
-    top: calc(100vw * (135 / 487));
-  }
+  z-index: 1;
 `
+
+const PlateImage = styled.img`
+  position: relative;
+  width: ${props => props.length};
+  height: auto;
+`
+
+const StandImage = styled.img`
+  position: absolute;
+  width: ${props => (props.isFirstRow ? 'calc(100vw * (67/ 1920))' : 'calc(100vw * (28 / 1920))')};
+  height: auto;
+  bottom: ${props => (props.isFirstRow ? '-60%' : props.adjustment)};
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 5;
+`
+
+const PlateStandContainer = styled.div`
+  position: relative;
+  z-index: ${props => props.zIndex};
+`
+
+const Cupcake = memo(({ tier }) => {
+  let svgSrc = null
+  let length = 0
+  switch (tier) {
+    case 'platinum':
+      svgSrc = platpastry
+      length = 'calc(100vw * (174 / 1920))'
+      break
+    case 'gold':
+      svgSrc = goldpastry
+      length = 'calc(100vw * (200 / 1920))'
+      break
+    case 'silver':
+      svgSrc = silverpastry
+      length = 'calc(100vw * (178 / 1920))'
+      break
+    case 'bronze':
+      svgSrc = bronzepastry
+      length = 'calc(100vw * (204 / 1920))'
+      break
+    case 'startup':
+      svgSrc = startuppastry
+      length = 'calc(100vw * (170 / 1920))'
+      break
+    default:
+      svgSrc = startuppastry
+      length = 'calc(100vw * (170 / 1920))'
+  }
+  return <PastryImage src={svgSrc} length={length} />
+})
+
+const PlateStand = memo(({ tier, zIndex, isFirstRow }) => {
+  let svgSrc = null
+  let length = 0
+  let adjustment = '0%'
+  switch (tier) {
+    case 'platinum':
+      svgSrc = platplate
+      length = 'calc(100vw * (510 / 1920))'
+      adjustment = '-40%'
+      break
+    case 'gold':
+      svgSrc = goldplate
+      length = 'calc(100vw * (769 / 1920))'
+      adjustment = '10%'
+      break
+    case 'silver':
+      svgSrc = silverplate
+      length = 'calc(100vw * (926 / 1920))'
+      adjustment = '25%'
+      break
+    case 'bronze':
+      svgSrc = bronzeplate
+      length = 'calc(100vw * (1220 / 1920))'
+      adjustment = '40%'
+      break
+    case 'startup':
+    default:
+      svgSrc = startupplate
+      length = 'calc(100vw * (1529 / 1920))'
+      adjustment = '45%'
+      break
+  }
+  const standSrc = isFirstRow ? startingstand : normalstand
+  return (
+    <PlateStandContainer zIndex={zIndex}>
+      <StandImage src={standSrc} adjustment={adjustment} isFirstRow={isFirstRow} />
+      <PlateImage src={svgSrc} length={length} />
+    </PlateStandContainer>
+  )
+})
 
 const Sponsor = memo(({ link, url, size, tier }) => (
   <SponsorContainer size={size} tier={tier}>
     <SponsorLink href={link} target="_blank" rel="noreferrer">
+      <Cupcake tier={tier} />
       <SponsorImg src={url} alt="Sponsor Logo" />
     </SponsorLink>
   </SponsorContainer>
 ))
 
-const ListByTier = memo(({ listOfRows, tierSize, tier }) => {
+const ListByTier = memo(({ listOfRows, tierSize, tier, startIndex }) => {
   if (!listOfRows || listOfRows.length === 0) return null
 
   return (
     <>
-      {listOfRows.map(row => (
+      {listOfRows.map((row, index) => (
         <SponsorLevelContainer key={`${tier}-${row[0].name}`}>
-          <Floor />
-          {tier === 'title' && <Nugget src="./assets/images/nugget_sponsor.png" />}
+          <PlateStand tier={tier} zIndex={100 - startIndex - index} isFirstRow={startIndex + index === 5} />
           <Row>
             {row.map(item => (
               <Sponsor key={item.name} link={item.link} url={item.imgURL} size={tierSize} tier={tier} />
@@ -184,42 +292,55 @@ const SponsorsGrid = ({ sponsors }) => {
     return () => window.removeEventListener('resize', calculateRows)
   }, [tierList, isMobile])
 
+  const startIndexPlatinum = 5
+  const startIndexGold = startIndexPlatinum + tierList.platinum.length
+  const startIndexSilver = startIndexGold + tierList.gold.length
+  const startIndexBronze = startIndexSilver + tierList.silver.length
+  const startIndexStartup = startIndexBronze + tierList.bronze.length
+  const startIndexInkind = startIndexStartup + tierList.startup.length
+
   return (
     <Container ref={containerRef}>
-      <ListByTier
+      {/* <ListByTier
         listOfRows={rows.title}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.title : SPONSOR_WIDTH.title}
         tier="title"
-      />
+      /> */}
       <ListByTier
         listOfRows={rows.platinum}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.platinum : SPONSOR_WIDTH.platinum}
         tier="platinum"
+        startIndex={startIndexPlatinum}
       />
       <ListByTier
         listOfRows={rows.gold}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.gold : SPONSOR_WIDTH.gold}
         tier="gold"
+        startIndex={startIndexGold}
       />
       <ListByTier
         listOfRows={rows.silver}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.silver : SPONSOR_WIDTH.silver}
         tier="silver"
+        startIndex={startIndexSilver}
       />
       <ListByTier
         listOfRows={rows.bronze}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.bronze : SPONSOR_WIDTH.bronze}
         tier="bronze"
+        startIndex={startIndexBronze}
       />
       <ListByTier
         listOfRows={rows.startup}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.startup : SPONSOR_WIDTH.startup}
         tier="startup"
+        startIndex={startIndexStartup}
       />
       <ListByTier
         listOfRows={rows.inkind}
         tierSize={isMobile ? MOBILE_SPONSOR_WIDTH.inkind : SPONSOR_WIDTH.inkind}
         tier="inkind"
+        startIndex={startIndexInkind}
       />
     </Container>
   )
