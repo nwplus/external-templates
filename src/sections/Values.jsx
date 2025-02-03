5e275afffd883f14660fa7e84ce538dc882358f8
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-02-02 21:57:56 -0800

    test diff timing

--- src/sections/Values.jsx
+++ src/sections/Values.jsx
@@ -235,7 +235,7 @@ const Values = () => {
 
           setTimeout(() => {
             setDescValuesEnabled(true);
-          }, 20); // Adjust delay time here
+          }, 48); // Adjust delay time here
 
           // setTimeout(() => {
           //   setValuesEnabled(true);
