55a6bd6fae86d58a0b4d242cdcdb164cc61803a2
kashgarg <kashishgarg247@gmail.com>
2025-02-04 18:41:25 -0800

    Tracks section w/o animation

--- src/sections/Tracks.jsx
+++ src/sections/Tracks.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Body, Header2 } from '@components/Typography'
-import Healthcare from '@assets/images/card_healthcare.svg'
-import Community from '@assets/images/card_community.svg'
-import Sustainability from '@assets/images/card_sustainability.svg'
-import Wildcard from '@assets/images/card_wildcard.svg'
+import { Header2 } from '@components/Typography'
+import Accessibility from '@assets/images/tracks/accessibility_track.png'
+import Wellness from '@assets/images/tracks/wellness_track.png'
+import Social from '@assets/images/tracks/social_track.png'
+import Wildcard from '@assets/images/tracks/wildcard_track.png'
+import Arrow from '@assets/images/tracks/arrow.svg'
 
 const TrackContainer = styled.div`
   position: relative;
+  aspect-ratio: 1920 / 1000;
   z-index: 1;
   display: flex;
   align-items: center;
   width: 100%;
   ${p => p.theme.mediaQueries.mobile} {
+    aspect-ratio: 393 / 1690;
     margin-top: 3rem;
   }
 `
@@ -35,83 +38,122 @@ const TrackInner = styled.div`
   }
 `
 
-const TextElements = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 60%;
-
-  ${p =>
-    p && p.bias && p.bias === 'left'
-      ? `
-    margin-right: auto;
-  `
-      : `
-    margin-left: auto;
-  `}
-
-  ${p => p.theme.mediaQueries.mobile} {
-    max-width: 100vw;
-    margin: 0;
-    padding: 0 6vw;
-    margin-top: 0%;
-  }
-`
-
 const Header = styled(Header2)`
-  color: #f3f5f4;
+  position: absolute;
+  top: calc(100vw * (150 / 1000));
+  left: calc(100vw * (1050 / 1920));
+  transform: rotate(-7.243deg);
+  color: #A6321E;
+  text-align: center;
+  font-family: Gloock;
+  font-size: calc(100vw * (54 / 1920));
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 
   ${p => p.theme.mediaQueries.mobile} {
-    font-size: 1.5rem;
-    margin-top: 1rem;
-    width: 100%;
-    text-align: left;
-  }
-`
-
-const Description = styled(Body)`
-  padding-top: 2rem;
-  font-weight: normal;
-  font-size: 1.1rem;
-  line-height: 150%;
-  color: #f3f5f4;
-
-  ${p => p.theme.mediaQueries.mobile} {
-    font-size: 1rem;
-    padding-top: 0.4rem;
-    width: 100%;
-    text-align: left;
+    font-size: calc(100vw * (30 / 393));
+    color: #A6321E;
+    left: calc(100vw * (0 / 393));
+    right: calc(100vw * (0 / 393));
+    margin: calc(100vw * (0 / 393)) auto;
+    top: calc(100vw * (30 / 393));
+    transform: rotate(0deg);
   }
 `
 
 const Instruction = styled.h3`
-  padding-top: 8rem;
-  font-weight: bold;
-  font-size: 1.5rem;
-  line-height: 150%;
-  color: #f3f5f4;
-`
-
-const CardGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  gap: 3rem;
-  width: fit-content;
-`
-
-const CardImg = styled.img`
-  box-shadow: #0f212c -12px 12px;
-  border-radius: 15px;
-  transition: 300ms;
+  position: absolute;
+  top: calc(100vw * (350 / 1000));
+  left: calc(100vw * (575 / 1920));
+  color: #000;
+  transform: rotate(2.866deg);
+  text-align: center;
+  font-family: Poppins;
+  font-size: calc(100vw * (25 / 1920));
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 
   ${p => p.theme.mediaQueries.mobile} {
-    width: 200px;
-    margin-bottom: 1rem;
+    transform: rotate(0deg);
+    font-size: calc(100vw * (17 / 393));
+    left: calc(100vw * (0 / 393));
+    right: calc(100vw * (0 / 393));
+    margin: calc(100vw * (0 / 393)) auto;
+    top: calc(100vw * (80 / 393));
   }
 `
 
-const MobileContent = styled.div`
-  width: 100%;
+const AccessibilityCardImg = styled.img`
+  position: absolute;
+  width: calc(100vw * (390 / 1920));
+  top: calc(100vw * (125 / 1000));
+  left: calc(100vw * (50 / 1920));
+
+  ${p => p.theme.mediaQueries.mobile} {
+    width: calc(100vw * (800 / 1000));
+    top: calc(100vw * (290 / 1000));
+    left: calc(100vw * (100 / 1920));
+    z-index: 1;
+  }
+`
+
+const WellnessCardImg = styled.img`
+  position: absolute;
+  width: calc(100vw * (390 / 1920));
+  top: calc(100vw * (40 / 1000));
+  left: calc(100vw * (525 / 1920));
+
+  ${p => p.theme.mediaQueries.mobile} {
+    width: calc(100vw * (800 / 1000));
+    top: calc(100vw * (1220 / 1000));
+    left: calc(100vw * (280 / 1920));
+    z-index: 2;
+  }
+`
+
+const SocialCardImg = styled.img`
+  position: absolute;
+  width: calc(100vw * (420 / 1920));
+  top: calc(100vw * (192 / 1000));
+  left: calc(100vw * (970 / 1920));
+
+  ${p => p.theme.mediaQueries.mobile} {
+    width: calc(100vw * (850 / 1000));
+    top: calc(100vw * (2230 / 1000));
+    left: calc(100vw * (100 / 1920));
+    z-index: 3;
+  }
+`
+
+const WildcardCardImg = styled.img`
+  position: absolute;
+  width: calc(100vw * (390 / 1920));
+  top: calc(100vw * (65 / 1000));
+  left: calc(100vw * (1450 / 1920));
+
+  ${p => p.theme.mediaQueries.mobile} {
+    width: calc(100vw * (800 / 1000));
+    top: calc(100vw * (3150 / 1000));
+    left: calc(100vw * (220 / 1920));
+    z-index: 4;
+  }
+`
+
+const ArrowImg = styled.img`
+  position: absolute;
+  width: calc(100vw * (60 / 1920));
+  top: calc(100vw * (340 / 1000));
+  left: calc(100vw * (505 / 1920));
+
+  ${p => p.theme.mediaQueries.mobile} {
+    width: calc(100vw * (200 / 393));
+    margin-bottom: calc(100vw * (16 / 393));
+  }
+`
+
+const MobileContent = styled.div`  width: 100%;
   height: 100%;
   flex-direction: column;
   gap: 5rem;
@@ -121,12 +163,6 @@ const MobileContent = styled.div`
   ${p => p.theme.mediaQueries.mobile} {
     display: flex;
   }
-`
-const MobileItem = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `
 
 const DesktopContent = styled.div`
@@ -141,106 +177,31 @@ const DesktopContent = styled.div`
   }
 `
 
-const CONTENT = {
-  default: {
-    heading: 'What can I make at nwHacks?',
-    description: (
-      <Description>
-        This year, we’re introducing nwTracks! We’ll be rewarding the top projects in each category. You can submit to
-        just one of the tracks - choose wisely!
-      </Description>
-    ),
-  },
-  healthcare: {
-    heading: 'Healthcare & Accessibility',
-    description: (
-      <Description>
-        Whether it&apos;s developing assistive technologies, improving healthcare accessibility, promoting mental health
-        awareness, or supporting wellbeing, this track is all about fostering a world where everyone can thrive.
-      </Description>
-    ),
-    card: Healthcare,
-  },
-  sustainability: {
-    heading: 'Sustainability & Environment',
-    description: (
-      <Description>
-        Find creative solutions to pressing environmental issues, including climate change, conservation, and
-        sustainable practices. Develop technologies that can contribute to a greener and more sustainable future!
-      </Description>
-    ),
-    card: Sustainability,
-  },
-  community: {
-    heading: 'Community & Connection',
-    description: (
-      <Description>
-        Explore ways to bring people closer and foster connection (in-person or online!). Build online communities,
-        virtual spaces or social platforms that strengthen bonds.
-      </Description>
-    ),
-    card: Community,
-  },
-  wildcard: {
-    heading: 'Wild Card',
-    description: (
-      <Description>
-        Let your creativity run wild! There are no boundaries or limitations here; you&apos;re free to come up with
-        innovative ideas that might not fit neatly into traditional categories. Explore new technologies, try out a new
-        API, and build something unique!
-      </Description>
-    ),
-    card: Wildcard,
-  },
-}
+const Track = () => (
 
-const Track = () => {
-  const [selection, setSelection] = useState('default')
-
-  const getProps = (key, hover) => ({
-    src: CONTENT[key].card,
-    alt: CONTENT[key].heading,
-    onMouseOver: () => {
-      setSelection(key)
-    },
-    style: { filter: hover && !['default', key].includes(selection) ? 'grayscale(1)' : undefined },
-  })
-
-  return (
     <TrackContainer id="tracks">
       <TrackInner>
         <MobileContent>
-          {Object.entries(CONTENT).map(([k, v]) => (
-            <MobileItem>
-              {v.card && <CardImg {...getProps(k)} />}
-              <Header style={{ marginTop: '1rem' }}>{v.heading}</Header>
-              <Description>{v.description}</Description>
-            </MobileItem>
-          ))}
+          <Header>Tracks</Header>
+          <Instruction>Click to learn more!</Instruction>
+          <AccessibilityCardImg src={Accessibility} alt="Accessibility" />
+          <WellnessCardImg src={Wellness} alt="Wellness" />
+          <SocialCardImg src={Social} alt="Social" />
+          <WildcardCardImg src={Wildcard} alt="Wildcard" />
         </MobileContent>
 
         <DesktopContent>
-          <TextElements bias="left">
-            <Header>{CONTENT[selection].heading}</Header>
-            {CONTENT[selection].description}
-
-            {selection === 'default' && <Instruction>▶ Hover over each card to learn more!</Instruction>}
-          </TextElements>
-
-          <CardGrid
-            onMouseLeave={() => {
-              setSelection('default')
-            }}
-          >
-            <CardImg {...getProps('healthcare', true)} />
-            <CardImg {...getProps('sustainability', true)} />
-            <CardImg {...getProps('community', true)} />
-            <CardImg {...getProps('wildcard', true)} />
-          </CardGrid>
+          <Header>Tracks</Header>
+          <Instruction>Click to learn more!</Instruction>
+          <ArrowImg src={Arrow} alt="Arrow" />
+          <AccessibilityCardImg src={Accessibility} alt="Accessibility" />
+          <WellnessCardImg src={Wellness} alt="Wellness" />
+          <SocialCardImg src={Social} alt="Social" />
+          <WildcardCardImg src={Wildcard} alt="Wildcard" />
         </DesktopContent>
       </TrackInner>
     </TrackContainer>
   )
-}
 
 export default Track
+
