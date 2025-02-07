0e5506a70d9b771e0c27ba12eb1dcbc8f97bcce5
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-02-07 11:49:42 -0800

    dynamic text size for gallery tool tip

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -318,13 +318,13 @@ const TooltipText = styled.div`
   text-align: center;
   font-feature-settings: 'liga' off;
   font-family: "Poppins";
-  font-size: 24px;
+  font-size: calc(100vw * (30 / 1920));;
   font-style: normal;
   font-weight: 600;
   line-height: 23px;
   letter-spacing: -0.15px;
   position: absolute;
-  top: calc(100vw * (30 / 1920));
+  top: calc(100vw * (40 / 1920));
   left: calc(100vw * (1200 / 1920));
 `
 
@@ -332,7 +332,7 @@ const TooltipImg = styled.img`
   position: absolute;
   height: auto;
   width: calc(100vw * (40 / 1920));
-  top: calc(100vw * (20 / 1920));
+  top: calc(100vw * (40 / 1920));
   left: calc(100vw * (1135 / 1920)); 
 `
 
