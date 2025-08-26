576854d11223fa1b72c53815bc8127908736cf17
Khaedra <g.jiang06@gmail.com>
2025-08-26 11:21:18 -0700

    linting errors

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex justify-between items-center px-25 mt-10">
       <div className="flex gap-15 px-3 py-6 text-[1.3rem] font-extrabold">
-        <a href="/" className="">
+        <a href="/about" className="">
           About
         </a>
         <a href="/about" className="">
