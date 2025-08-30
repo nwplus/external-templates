e2d7b951c6f07d3de13bf96ff7cf3559e5832b19
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2025-08-30 15:51:03 -0700

    Add countdown signs

--- src/components/hero/sign.tsx
+++ src/components/hero/sign.tsx
@@ -1,48 +1,21 @@
-import { cn } from "@/lib/utils";
-
 import Image from "next/image";
 
-const SignButton = ({
-  children,
+const Sign = ({
+  number,
+  unit,
   className,
 }: {
-  children: React.ReactNode;
+  number: number;
+  unit: string;
   className: string;
 }) => {
   return (
-    <button
-      className={cn(
-        "absolute bg-[#2C6D7D] text-[1vw] py-[0.5vw] px-[1vw] rounded-[0.5vw] text-title cursor-pointer hover:opacity-80 transition-opacity",
-        className
-      )}
-    >
-      {children}
-    </button>
-  );
-};
-
-const Sign = () => {
-  return (
-    <div>
-      <Image
-        src="/assets/hero/sign.png"
-        alt="sign"
-        width={1200}
-        height={800}
-        className="pointer-events-none"
-      />
-      <div className="absolute left-1/2 -translate-x-1/2 top-[20%] flex flex-col ml-[1vw] items-center font-title text-title">
-        <h1 className="text-[6.8vw] text-shadow-lg leading-tight">HackCamp</h1>
-        <h2 className="text-[1.5vw]">
-          Canada&apos;s largest beginner-only hackathon
-        </h2>
+    <div className={className}>
+      <Image src="/assets/hero/sign.svg" alt="sign" width={350} height={200} />
+      <div className="absolute left-1/2 -translate-x-1/2 top-[3.5vw] flex flex-col gap-[2vw] items-center font-title text-title">
+        <p className="text-[4.5vw] leading-[0.1vw]">{number}</p>
+        <p className="text-[1.5vw]">{unit}</p>
       </div>
-      <SignButton className="left-[22.6vw] top-[19.8vw]">
-        Register Now
-      </SignButton>
-      <SignButton className="right-[20.5vw] top-[19.8vw]">
-        Become a Mentor
-      </SignButton>
     </div>
   );
 };
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import EntranceSign from "@/components/hero/entrance-sign";
 import Sign from "@/components/hero/sign";
 
+import Image from "next/image";
 import React from "react";
 
 import Navbar from "../components/hero/navbar";
@@ -13,8 +15,39 @@ export default function Hero() {
       <div className="relative flex flex-col bg-[url('/assets/hero/background-1.svg')] bg-cover bg-center bg-no-repeat h-[116.56vw] pt-4">
         <Navbar />
         <div className="absolute left-1/2 -translate-x-1/2 top-[10vw] w-[65vw]">
-          <Sign />
+          <EntranceSign />
+          <Image
+            src="/assets/hero/hackathon-info-left.svg"
+            alt="In-person event"
+            title="In-person event"
+            width={350}
+            height={200}
+            className="absolute left-0 bottom-0 w-[35%]"
+          />
+          <Image
+            src="/assets/hero/hackathon-info-right.svg"
+            alt="Nov 15: Learn Day; Nov 15-16: Build Night"
+            title="Nov 15: Learn Day; Nov 15-16: Build Night"
+            width={400}
+            height={400}
+            className="absolute -right-[5vw] -bottom-[5vw] w-[35%]"
+          />
         </div>
+        <Sign
+          number={12}
+          unit="days"
+          className="absolute left-[8vw] top-[38vw] w-[9vw]"
+        />
+        <Sign
+          number={10}
+          unit="hours"
+          className="absolute left-[15.5vw] top-[41vw] w-[9vw]"
+        />
+        <Sign
+          number={24}
+          unit="minutes"
+          className="absolute left-[23vw] top-[45vw] w-[9vw]"
+        />
       </div>
     </div>
   );
