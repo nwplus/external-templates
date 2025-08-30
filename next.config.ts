4143ddce69208214a87983bd760ef67064887430
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2025-08-30 13:04:41 -0700

    Add faq grass buffer

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    // Must disable for output: "export"
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
