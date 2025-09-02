e3b6efae702e9f8b84a7c96a407b177bddd4db12
Kezia <roselinakeziarijadi@dhcp-128-189-232-202.ubcsecure.wireless.ubc.ca>
2025-09-02 00:22:26 -0700

    fix: 10 years link

--- src/sections/ten-years.tsx
+++ src/sections/ten-years.tsx
@@ -61,7 +61,6 @@ export default function TenYears() {
     <div
       ref={containerRef}
       className="relative h-[400vh] z-20 -mt-[100vh] pointer-events-none"
-      id="10-years"
     >
       <motion.div
         className="sticky top-0 h-screen overflow-hidden pointer-events-none"
@@ -139,7 +138,7 @@ export default function TenYears() {
                       </div>
 
                       <div className="space-y-6">
-                        <div className="relative w-full max-w-md">
+                        <div className="relative w-full max-w-md" id="10-years">
                           <Image
                             src="/assets/ten-years/graphics/image-background.svg"
                             alt="Image background"
