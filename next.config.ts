3a2e51c1763d3b43a4e4985b8e12b8575e5f9b61
trisha <82386753+tdanielles@users.noreply.github.com>
2025-10-02 21:27:19 -0700

    ?

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // Must disable for output: "export"
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
