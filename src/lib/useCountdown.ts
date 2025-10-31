1731c04b60f0e371db6309667d097aa7d05f868c
ErpingS <erpingsun@gmail.com>
2025-10-31 16:44:57 -0700

    fix lint

--- src/lib/useCountdown.ts
+++ src/lib/useCountdown.ts
@@ -1,38 +1,38 @@
 import { useEffect, useState } from "react";
 
 const getReturnValues = (countdown: number) => {
-	if (countdown < 0) {
-		return { days: 0, hours: 0, minutes: 0 };
-	}
+  if (countdown < 0) {
+    return { days: 0, hours: 0, minutes: 0 };
+  }
 
-	// calculate time left
-	const days = Math.floor(countdown / (1000 * 60 * 60 * 24)).toString();
-	const hours = twoify(
-		Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-	);
-	const minutes = twoify(
-		Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60))
-	);
+  // calculate time left
+  const days = Math.floor(countdown / (1000 * 60 * 60 * 24)).toString();
+  const hours = twoify(
+    Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const minutes = twoify(
+    Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60))
+  );
 
-	return { days, hours, minutes };
+  return { days, hours, minutes };
 };
 
 export const useCountdown = () => {
-	const countDownDate = new Date("Nov 15, 2025 9:00:00").getTime();
-	const [countDown, setCountDown] = useState(countDownDate - Date.now());
+  const countDownDate = new Date("Nov 15, 2025 9:00:00").getTime();
+  const [countDown, setCountDown] = useState(countDownDate - Date.now());
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setCountDown(countDownDate - Date.now());
-		}, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - Date.now());
+    }, 1000);
 
-		return () => clearInterval(interval);
-	}, [countDownDate]);
+    return () => clearInterval(interval);
+  }, [countDownDate]);
 
-	return getReturnValues(countDown);
+  return getReturnValues(countDown);
 };
 
 const twoify = (num: number) => {
-	const str = num.toString();
-	return str.length === 1 ? `0${str}` : str;
+  const str = num.toString();
+  return str.length === 1 ? `0${str}` : str;
 };
