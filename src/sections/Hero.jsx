84c0e44d0076490e76f93b4d4f221604727edff3
trisha <82386753+tdanielles@users.noreply.github.com>
2025-12-01 21:51:34 +0800

    nwHacks Website fixes for Hero, About, and Navbar (#648)
    * fix mobile abt button not working
    
    * add mentor and volunteer app form links to hero
    
    * fix island sizing for mobile
    
    * actual fix to island

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -203,7 +203,7 @@ const HeroIslandWrapper = styled.div`
   top: calc(100vw * (170 / 1512));
   right: calc(100vw * (120 / 1512));
 
-  height: clamp(400px, calc(100vw * (778 / 1512)), 1000px);
+  height: clamp(300px, calc(100vw * (778 / 1512)), 1000px);
   overflow: hidden;
 
   ${p => p.theme.mediaQueries.mobile} {
@@ -211,7 +211,7 @@ const HeroIslandWrapper = styled.div`
     top: calc(100vw * (1600 / 1512));
     left: 50%;
     transform: translateX(-50%);
-    width: calc(100vw * (338 / 528));
+    width: calc(100vw * (310 / 528));
     z-index: 2;
     height: auto;
   }
@@ -334,9 +334,21 @@ const Hero = () => (
         </Description>
         <Subheader>Apply to be a:</Subheader>
         <Buttons>
-          <HackerButton href="https://portal.nwplus.io/">Hacker</HackerButton>
-          <MentorButton>Mentor</MentorButton>
-          <VolunteerButton>Volunteer</VolunteerButton>
+          <HackerButton href="https://portal.nwplus.io/" target="_blank">
+            Hacker
+          </HackerButton>
+          <MentorButton
+            href="https://docs.google.com/forms/d/e/1FAIpQLSed4X4CvZI3NoeOF5xjhMEISiM3RR6W96reEB-uFn_3IUVhzg/viewform"
+            target="_blank"
+          >
+            Mentor
+          </MentorButton>
+          <VolunteerButton
+            href="https://docs.google.com/forms/d/e/1FAIpQLScTRJmFJLBG8fGFSlSBJmR_O2Jk2_mZjZ-EBNYpoFhrkoczFg/viewform"
+            target="_blank"
+          >
+            Volunteer
+          </VolunteerButton>
         </Buttons>
         <SponsorText href="mailto:sponsorship@nwplus.io?subject=Sponsorship Inquiry">Become a Sponsor</SponsorText>
       </ContentSide>
