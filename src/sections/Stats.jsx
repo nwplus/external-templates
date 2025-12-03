8883221a4acd9a61c56abe7da28476206744ce19
martincai8 <martincai46@gmail.com>
2025-12-03 15:48:45 -0800

    fix link

--- src/sections/Stats.jsx
+++ src/sections/Stats.jsx
@@ -623,7 +623,7 @@ const Stats = () => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/watch?v=TtYBTPVJCwo"
+            src="https://www.youtube.com/embed/TtYBTPVJCwo?si=_CGjDyKyKvEIIEHS"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
