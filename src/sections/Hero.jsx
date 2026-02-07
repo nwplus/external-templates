1f039a26b1b53ddb26c3e71a769408a7a2f52f19
Lincoln Lee <Lsha0730@gmail.com>
2026-02-07 02:08:48 -0800

    minor: change hero button text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -264,7 +264,7 @@ const Hero = () => (
 
       <ButtonContainer>
         <Button className="primary" href={LINKS.mentor} target="_blank" rel="noopener noreferrer">
-          Become a Mentor
+          Become a Mentor or Judge
         </Button>
         <Button className="secondary" href={LINKS.apply} target="_blank" rel="noopener noreferrer">
           Apply Now
