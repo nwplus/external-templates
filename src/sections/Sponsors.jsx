e355e725a094ff1242baf5dae1c905919a934c47
ErpingS <erpingsun@gmail.com>
2026-02-10 12:02:58 -0800

    cmd-f 2026 sponsors mobile

--- src/sections/Sponsors.jsx
+++ src/sections/Sponsors.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { SCREEN_BREAKPOINTS } from 'src/theme/ThemeProvider'
 import styled from 'styled-components'
 import { Header2 } from '@components/Typography'
 import SponsorsGrid from '@components/SponsorsGrid'
@@ -17,9 +16,7 @@ const SponsorsContainer = styled.div`
   margin-top: calc(100vw * (200 / 1920));
 
   ${p => p.theme.mediaQueries.mobile} {
-    margin-top: calc(100vw * (-150 / 393));
-    aspect-ratio: 393 / 800;
-  }
+    margin-bottom: calc(100vw * (150 / 393));
 `
 
 const Title = styled(Header2)`
@@ -105,24 +102,6 @@ const SponsorButton = styled.button`
 const Sponsors = () => {
   const [sponsors, setSponsors] = useState([])
   const [carouselSponsors, setCarouselSponsors] = useState([])
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= SCREEN_BREAKPOINTS.mobile)
-    }
-
-    if (typeof window !== 'undefined') {
-      handleResize()
-      window.addEventListener('resize', handleResize)
-    }
-
-    return () => {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('resize', handleResize)
-      }
-    }
-  }, [])
 
   useEffect(async () => {
     const data = await fireDb.getCollection('cmd-f2026', 'Sponsors')
@@ -163,19 +142,8 @@ const Sponsors = () => {
         Sponsor us!
       </SponsorButton>
       {/* <SwipeDescription>Swipe on the TV screen to read about our sponsors</SwipeDescription> */}
-      {!isMobile ? (
-        <>
-          {carouselSponsors.length > 0 && <Carousel sponsors={carouselSponsors} />}
-          <SponsorsGrid sponsors={sponsors} />
-        </>
-      ) : (
-        <>
-          <Description>
-            To view our current full list of sponsors, please view this website on desktop. We are still working on the
-            mobile version!
-          </Description>
-        </>
-      )}
+      {carouselSponsors.length > 0 && <Carousel sponsors={carouselSponsors} />}
+      <SponsorsGrid sponsors={sponsors} />
     </SponsorsContainer>
   )
 }
