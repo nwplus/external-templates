76deaae5f67017b0b2a90f35dc4182e6b583956a
naijwu <jaewuchun@gmail.com>
2026-09-12 09:38:56 -0700

    chore: navbar updates

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -8,10 +8,12 @@ import Section2 from "./section-2";
 export default function Hero() {
   return (
     <>
+      {/* Desktop */}
       <div className="hidden md:block">
         <Section1 />
         <Section2 className="-mt-[25vw]" />
       </div>
+      {/* Mobile */}
       <div className="block md:hidden">
         <HeroMobile />
         <LearnMobile />
--- src/sections/hero/section-1.tsx
+++ src/sections/hero/section-1.tsx
@@ -8,6 +8,9 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 import Navbar from "../../components/hero/navbar";
+import { HeroClouds } from "@/components/hero/clouds";
+import { HeroDecals } from "@/components/hero/decals";
+import { HeroCloudsInner } from "@/components/hero/clouds-inner";
 
 const carImages = [
   {
@@ -80,75 +83,14 @@ const Section1 = () => {
 
   // Container min-height based on background image aspect ratio (2238÷1920 = 116.56vw)
   return (
-    <div className="relative flex flex-col bg-hero-1 bg-cover bg-center bg-no-repeat h-[116.56vw] pt-4">
+    <div className="bg-linear-to-b from-[#0B0F27] to-[#28418D]">
       <Navbar />
-      <div className="absolute left-1/2 -translate-x-1/2 top-[10vw] w-[65vw]">
-        <EntranceSign />
-        <Image
-          src="/assets/hero/hackathon-info-left.svg"
-          alt="In-person event"
-          title="In-person event"
-          width={350}
-          height={200}
-          className="absolute left-0 bottom-0 w-[35%]"
-        />
-        <Image
-          src="/assets/hero/hackathon-info-right.svg"
-          alt="Nov 7: Learn Day; Nov 7-8: Build Night"
-          title="Nov 7: Learn Day; Nov 7-8: Build Night"
-          width={400}
-          height={400}
-          className="absolute -right-[5vw] -bottom-[5vw] w-[35%]"
-        />
-      </div>
-      <Sign
-        number={days}
-        unit="days"
-        className="absolute left-[8vw] top-[38vw] w-[9vw]"
-      />
-      <Sign
-        number={hours}
-        unit="hours"
-        className="absolute left-[15.5vw] top-[41vw] w-[9vw]"
-      />
-      <Sign
-        number={minutes}
-        unit="minutes"
-        className="absolute left-[23vw] top-[45vw] w-[9vw]"
-      />
-      {carImages.map((car, index) => (
-        <Image
-          key={index}
-          src={car.src}
-          alt={car.alt}
-          className="absolute"
-          width={250}
-          height={250}
-          style={{
-            left: car.left,
-            top: car.top,
-            width: car.width,
-            opacity: getCarOpacity(car.scrollRange),
-            transition: "opacity 0.3s ease-out",
-          }}
-        />
-      ))}
-      <div className="absolute left-[7.5vw] top-[62.5vw] w-[37vw] flex flex-col gap-[1.5vw]">
-        <h2 className="text-[3.5vw] font-title leading-none text-shadow-bold">
-          Welcome to HackCamp
-        </h2>
-        <p className="text-[1.2vw]">
-          HackCamp provides a space for hundreds of first-time hackers curious
-          about technology to explore the field further through hands-on
-          learning, regardless of whether you have coding experience or not!
-        </p>
-        <p className="text-[1.2vw]">
-          Over the past 11 years, HackCamp has revolved around accessibility,
-          inclusivity, and diversity. We strive to help people break into
-          hackathon spaces by providing beginner-oriented workshops, industry
-          connections, encouraging you to bring your unique perspectives and
-          experiences to build your own project.
-        </p>
+      <div className="relative w-full">
+        <HeroClouds />
+        <div className="absolute inset-x-0 top-0">
+          <HeroCloudsInner />
+        </div>
+        {/* <HeroDecals /> */}
       </div>
     </div>
   );
